aa81cfe33b693d4a80cdd5fce08f31f011183afe
Maciej Małecki <me@mmalecki.com>
2014-04-16 16:45:32 +0200

    Make idempotent send work, together with a test

--- test/unit/traderSendTest.js
+++ test/unit/traderSendTest.js
@@ -12,7 +12,11 @@ var psqlInterface = new PostgresqlInterface(db);
 var TRANSACTION_FEE = 1;
 var FINGERPRINT = 'CB:3D:78:49:03:39:BA:47:0A:33:29:3E:31:25:F7:C6:4F:74:71:D7';
 var TXID = '216dabdb692670bae940deb71e59486038a575f637903d3c9af601ddd48057fc';
+var ADDRESS = '1LhkU2R8nJaU8Zj6jB8VjWrMpvVKGqCZ64';
+var SATOSHIS = 1337;
 var CURRENCY = 'USD';
+
+var OUR_TXID = uuid();
 
 describe('trader/send', function () {
   var trader = new Trader(psqlInterface);
@@ -27,14 +31,10 @@ describe('trader/send', function () {
   trader.pollRate = function () {};
 
   it('should call `sendBitcoins` on the transfer exchange', function (done) {
-    var address = '1LhkU2R8nJaU8Zj6jB8VjWrMpvVKGqCZ64';
-    var txId = uuid();
-    var satoshis = 1337;
-
     trader.transferExchange = {
-      sendBitcoins: function (address_, satoshis_, transactionFee, callback) {
-        assert.equal(address, address_);
-        assert.equal(satoshis, satoshis_);
+      sendBitcoins: function (address, satoshis, transactionFee, callback) {
+        assert.equal(ADDRESS, address);
+        assert.equal(SATOSHIS, satoshis);
         assert.equal(transactionFee, TRANSACTION_FEE);
         callback(null, TXID);
       }
@@ -42,10 +42,30 @@ describe('trader/send', function () {
 
     trader.sendBitcoins(FINGERPRINT, {
       fiat: 100,
-      txId: txId,
+      txId: OUR_TXID,
       currencyCode: CURRENCY,
-      toAddress: address,
-      satoshis: satoshis
+      toAddress: ADDRESS,
+      satoshis: SATOSHIS
+    }, function (err, txId) {
+      assert.notOk(err);
+      assert.equal(txId, TXID);
+      done();
+    });
+  });
+
+  it('should not call `sendBitcoins` on the transfer exchange with same send', function (done) {
+    trader.transferExchange = {
+      sendBitcoins: function () {
+        throw new Error('This should not have been called');
+      }
+    };
+
+    trader.sendBitcoins(FINGERPRINT, {
+      fiat: 100,
+      txId: OUR_TXID,
+      currencyCode: CURRENCY,
+      toAddress: ADDRESS,
+      satoshis: SATOSHIS
     }, function (err, txId) {
       assert.notOk(err);
       assert.equal(txId, TXID);
