71aa425dd116ec26981b423df44ae966da8316bc
Josh Harvey <josh@lamassu.is>
2015-01-08 23:11:03 -0500

    migration fix + removed some old incoming checking

--- migrations/002-bills.js
+++ migrations/002-bills.js
@@ -3,16 +3,22 @@
 var db = require('./db');
 
 exports.up = function(next) {
-  db.query('CREATE TABLE IF NOT EXISTS bills ( ' +
+  var sqls = [
+    'CREATE TABLE IF NOT EXISTS bills ( ' +
     'id uuid PRIMARY KEY, ' +
     'device_fingerprint text NOT NULL, ' +
     'denomination integer NOT NULL, ' +
     'currency_code text NOT NULL, ' +
     'satoshis integer NOT NULL, ' +
     'to_address text NOT NULL, ' +
-    'session_id uuid NOT NULL, ' +
+    'transaction_id uuid NOT NULL, ' +
     'device_time bigint NOT NULL, ' +
-    'created timestamp NOT NULL DEFAULT now() )', next);
+    'created timestamp NOT NULL DEFAULT now() )',
+
+    'ALTER TABLE bills RENAME COLUMN transaction_id TO session_id'
+  ];
+
+  db.multi(sqls, next);
 };
 
 exports.down = function(next) {
