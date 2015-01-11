d1c7da98e8ff2031163350b88fa2fd27f4d4ee0d
Josh Harvey <josh@lamassu.is>
2015-01-11 16:32:11 -0500

    fix bills migration

--- migrations/002-bills.js
+++ migrations/002-bills.js
@@ -3,22 +3,21 @@
 var db = require('./db');
 
 exports.up = function(next) {
-  var sqls = [
-    'CREATE TABLE IF NOT EXISTS bills ( ' +
+  var sql =
+    'CREATE TABLE bills ( ' +
     'id uuid PRIMARY KEY, ' +
     'device_fingerprint text NOT NULL, ' +
     'denomination integer NOT NULL, ' +
     'currency_code text NOT NULL, ' +
     'satoshis integer NOT NULL, ' +
     'to_address text NOT NULL, ' +
-    'transaction_id uuid NOT NULL, ' +
+    'session_id uuid NOT NULL, ' +
     'device_time bigint NOT NULL, ' +
-    'created timestamp NOT NULL DEFAULT now() )',
+    'created timestamp NOT NULL DEFAULT now() )';
 
-    'ALTER TABLE bills RENAME COLUMN transaction_id TO session_id'
-  ];
-
-  db.multi(sqls, next);
+  db.silentQuery('ALTER TABLE bills RENAME TO bills_old', function() {
+    db.query(sql, next);
+  });
 };
 
 exports.down = function(next) {
