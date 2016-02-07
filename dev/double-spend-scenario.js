5b5fc8b8104199de2dc19270c05453b676da584c
Josh Harvey <josh@lamassu.is>
2016-02-07 02:40:20 +0200

    fix postgres locking issue which could lead to rare double send

--- dev/double-spend-scenario.js
+++ dev/double-spend-scenario.js
@@ -28,13 +28,21 @@ db.addOutgoingTx(session, tx, function (err, res) {
   console.log(res)
 })
 
+db.addOutgoingTx(session, tx2, function (err, res) {
+  console.log('DEBUG2')
+  console.log(err)
+  console.log(res)
+})
+
+/*
 setTimeout(function () {
   db.addOutgoingTx(session, tx2, function (err, res) {
     console.log('DEBUG2')
     console.log(err)
     console.log(res)
   })
-}, 1000)
+}, 0)
+*/
 
 var bills = {
   uuid: 'c630338c-cd03-11e5-a9df-dbc9be2e9fbb',
