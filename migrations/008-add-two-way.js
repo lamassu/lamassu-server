808a8ab4a48cef8af8f39ea4c45a39a30e81d53d
Josh Harvey <josh@lamassu.is>
2016-05-07 02:07:50 +0300

    WIP

--- migrations/008-add-two-way.js
+++ migrations/008-add-two-way.js
@@ -1,9 +1,15 @@
 var db = require('./db')
 
+function singleQuotify (item) { return '\'' + item + '\'' }
+
 exports.up = function (next) {
+  var statuses = ['notSeen', 'published', 'authorized', 'confirmed', 'rejected']
+  .map(singleQuotify).join(',')
+
   var sql = [
+    'create type status_stage AS enum (' + statuses + ')',
     'alter table transactions add dispensed boolean NOT NULL DEFAULT false',
-    'alter table transactions add authorized boolean NOT NULL DEFAULT false'
+    'alter table transactions add status status_stage NOT NULL DEFAULT \'notSeen\''
   ]
   db.multi(sql, next)
 }
