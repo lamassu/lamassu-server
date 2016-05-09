014033b73e2e8be0a07a9f3d6ad02f334bcabd05
Josh Harvey <josh@lamassu.is>
2016-05-09 18:40:31 +0300

    implement confirmation sms, bug fixes

--- migrations/008-add-two-way.js
+++ migrations/008-add-two-way.js
@@ -10,6 +10,8 @@ exports.up = function (next) {
   var sql = [
     'create type status_stage AS enum (' + statuses + ')',
     'alter table transactions add dispensed boolean NOT NULL DEFAULT false',
+    'alter table transactions add notified boolean NOT NULL DEFAULT false',
+    'alter table transactions add redeem boolean NOT NULL DEFAULT false',
     'alter table transactions add status status_stage NOT NULL DEFAULT \'notSeen\''
   ]
   db.multi(sql, next)
