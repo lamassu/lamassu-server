0140e01c9fcb8a902babb7148246b812a37c9beb
Josh Harvey <josh@lamassu.is>
2016-06-01 02:35:29 +0300

    WIP

--- migrations/012-add-hd-path-serial.js
+++ migrations/012-add-hd-path-serial.js
@@ -4,12 +4,10 @@ exports.up = function (next) {
   var sql = [
     `create table cash_out_hds (
       session_id uuid PRIMARY KEY,
-      to_address text NOT NULL,
       crypto_code text NOT NULL,
-      hd_path_prefix text NOT NULL,
       hd_serial integer NOT NULL,
       created timestamptz NOT NULL default now(),
-      unique (crypto_code, hd_serial)
+      unique (crypto_code, hd_serial),
     )`
   ]
   db.multi(sql, next)
