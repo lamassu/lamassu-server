2413d69e9426f597f0f0ae6788a37468430cf402
Josh Harvey <josh@lamassu.is>
2016-08-23 14:07:04 +0200

    migrate-config script

---
+++ bin/migrate-config.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const pgp = require('pg-promise')()
+const psqlUrl = require('../lib/options').postgresql
+
+const db = pgp(psqlUrl)
+
+db.one('select data from user_config where type=$1', 'exchanges')
+.then(data => {
+  const config = data.data
+  const settings = config.exchanges.settings
+  const compliance = settings.compliance
+  const newConfig = {
+    global: {
+      cashInTransactionLimit: compliance.maximum.limit,
+      cashOutTransactionLimit: settings.fiatTxLimit,
+      cashInCommission: settings.commission,
+      cashOutCommission: settings.fiatCommission || settings.commission,
+      idVerificationEnabled: compliance.idVerificationEnabled,
+      idVerificationLimit: compliance.idVerificationLimit,
+      currency: settings.currency,
+      lowBalanceMargin: settings.lowBalanceMargin,
+      cartridges: settings.cartridges,
+      virtualCartridges: settings.virtualCartridges,
+      zeroConfLimit: settings.zeroConfLimit,
+      coins: settings.coins,
+      direction: 'cashIn'
+    }
+  }
+
+  db.none('insert into user_config (type, data) values ($1, $2)', ['global', newConfig])
+  .then(() => {
+    console.log('Success.')
+    process.exit(0)
+  })
+  .catch(err => {
+    console.log('Error: %s', err)
+    process.exit(1)
+  })
+})
