36ac2f3ba5e6994f8bebdeadded2e6ead32fda7e
Josh Harvey <josh@lamassu.is>
2017-03-16 07:50:41 +0200

    WIPP

--- migrations/022-add_cash_in_sent.js
+++ migrations/022-add_cash_in_sent.js
@@ -4,6 +4,10 @@ exports.up = function (next) {
   var sql = [
     'alter table cash_in_txs add column send boolean not null default false',
     'alter table cash_in_txs rename currency_code to fiat_code',
+    'alter table bills rename currency_code to fiat_code',
+    'alter table bills rename denomination to fiat',
+    'alter table bills drop column to_address',
+    'alter table bills drop column device_id',
     'alter table cash_out_txs rename currency_code to fiat_code'
   ]
   db.multi(sql, next)
