d5aab9dfd03d55ef438c62414230fd8ff4bf2da5
Josh Harvey <josh@lamassu.is>
2017-04-19 02:25:08 +0300

    add sendConfirmed

---
+++ migrations/026-add_send_confirmed.js
@@ -0,0 +1,17 @@
+var db = require('./db')
+
+exports.up = function (next) {
+  var sql = [
+    'alter table cash_in_txs add column send_confirmed boolean not null default false',
+    'alter table cash_in_txs add column device_time bigint not null',
+    'alter table cash_in_txs add column timedout boolean not null default false',
+    'alter table cash_in_txs add column send_time timestamptz',
+    'alter table cash_out_txs add column device_time bigint not null',
+    'alter table cash_out_txs add column timedout boolean not null default false'
+  ]
+  db.multi(sql, next)
+}
+
+exports.down = function (next) {
+  next()
+}
