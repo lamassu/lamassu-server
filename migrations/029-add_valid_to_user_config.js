55717dbc8decd8a1d23dd282e6c29d65bce430d8
Josh Harvey <josh@lamassu.is>
2017-04-26 01:39:58 +0300

    minor fixes

--- migrations/029-add_valid_to_user_config.js
+++ migrations/029-add_valid_to_user_config.js
@@ -2,6 +2,7 @@ var db = require('./db')
 
 exports.up = function (next) {
   var sql = [
+    'delete from user_config',
     'alter table user_config add column valid boolean not null'
   ]
   db.multi(sql, next)
