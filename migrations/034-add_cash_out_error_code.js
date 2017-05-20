f0fabddb5047c835a67a12bf9aca8518fbf34b9e
Josh Harvey <josh@lamassu.is>
2017-05-20 18:08:09 +0300

    add blockcypher [WIP]

--- migrations/034-add_cash_out_error_code.js
+++ migrations/034-add_cash_out_error_code.js
@@ -1,8 +1,12 @@
+var db = require('./db')
 
-exports.up = function(next){
-  next();
-};
+exports.up = function (next) {
+  var sql = [
+    'alter table cash_out_txs add column error_code text'
+  ]
+  db.multi(sql, next)
+}
 
-exports.down = function(next){
-  next();
-};
+exports.down = function (next) {
+  next()
+}
