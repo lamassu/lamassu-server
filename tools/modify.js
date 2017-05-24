bcf72a0285e2cc6d65ad97238479f99c3c592f7c
Josh Harvey <josh@lamassu.is>
2017-05-24 15:59:59 +0100

    add crypto defaults

--- tools/modify.js
+++ tools/modify.js
@@ -1,8 +1,7 @@
 const settingsLoader = require('../lib/settings-loader')
 
 const fields = [
-  settingsLoader.configDeleteField({crypto: 'global', machine: 'global'}, 'cashOutFee'),
-  settingsLoader.configDeleteField({crypto: 'global', machine: 'global'}, 'minTx')
+  settingsLoader.configDeleteField({crypto: 'ETH', machine: 'global'}, 'exchange')
 ]
 
 settingsLoader.modifyConfig(fields)
