143ce8a98ce6dabe8fdb71377c351f4857314d36
goga-m <goga.mamoulasvili@gmail.com>
2017-08-14 17:50:34 +0300

    Changes on customers & compliance_authorization tables

--- migrations/037-add_compliance_authorizations_table.js
+++ migrations/037-add_compliance_authorizations_table.js
@@ -2,13 +2,13 @@ var db = require('./db')
 
 exports.up = function(next) {
   const sql =
-    ["CREATE TYPE compliance_types  as enum ('manual', 'sanctions', 'sanctions_override') ",
-    'CREATE TABLE compliance_authorizations ( ' +
-    'id uuid PRIMARY KEY, ' +
-    'customer_id uuid REFERENCES customers (id), ' +
-    'compliance_type compliance_types NOT NULL,' +
-    'authorized_at timestamptz NOT NULL,' +
-    'authorized_by text REFERENCES user_tokens (token) )' ]
+    [`create type compliance_types  as enum ('manual', 'sanctions', 'sanctions_override') `,
+    `create table compliance_authorizations ( 
+    id uuid PRIMARY KEY, 
+    customer_id uuid REFERENCES customers (id), 
+    compliance_type compliance_types NOT NULL,
+    authorized_at timestamptz NOT NULL,
+    authorized_by text REFERENCES user_tokens (token) )` ]
 
 
   db.multi(sql, next)
