500edcf2794f810b1818cfd3ebab3c7fc406906c
Josh Harvey <joshmh@gmail.com>
2017-08-29 16:08:06 +0300

    Add transaction versioning, tx cancellation (#79)

--- migrations/037-add_compliance_authorizations_table.js
+++ migrations/037-add_compliance_authorizations_table.js
@@ -1,19 +1,18 @@
 var db = require('./db')
 
-exports.up = function(next) {
+exports.up = function (next) {
   const sql =
-    [`create type compliance_types  as enum ('manual', 'sanctions', 'sanctions_override') `,
-    `create table compliance_authorizations ( 
-    id uuid PRIMARY KEY, 
-    customer_id uuid REFERENCES customers (id), 
+    [ "create type compliance_types  as enum ('manual', 'sanctions', 'sanctions_override')",
+    `create table compliance_authorizations (
+    id uuid PRIMARY KEY,
+    customer_id uuid REFERENCES customers (id),
     compliance_type compliance_types NOT NULL,
     authorized_at timestamptz NOT NULL,
     authorized_by text REFERENCES user_tokens (token) )` ]
 
-
   db.multi(sql, next)
-};
+}
 
-exports.down = function(next) {
-  next();
-};
+exports.down = function (next) {
+  next()
+}
