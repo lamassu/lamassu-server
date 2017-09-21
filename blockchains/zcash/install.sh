80f31806dc3736630cd585cc70a9c8ab9f2cb6e1
Neal Conner <neal@lamassu.is>
2017-09-21 11:49:32 -0400

    Update zcash to 1.0.11

--- blockchains/zcash/install.sh
+++ blockchains/zcash/install.sh
@@ -9,9 +9,9 @@ ufw allow 8233 >> $LOG_FILE 2>&1
 ufw allow 22 >> $LOG_FILE 2>&1
 ufw --force enable >> $LOG_FILE 2>&1
 ufw status >> $LOG_FILE 2>&1
-wget -q https://z.cash/downloads/zcash-1.0.10-1-linux64.tar.gz >> $LOG_FILE 2>&1
-tar -xzf zcash-1.0.10-1-linux64.tar.gz >> $LOG_FILE 2>&1
-cp zcash-1.0.10-1/bin/* /usr/local/bin >> $LOG_FILE 2>&1
+wget -q https://z.cash/downloads/zcash-1.0.11-linux64.tar.gz >> $LOG_FILE 2>&1
+tar -xzf zcash-1.0.11-linux64.tar.gz >> $LOG_FILE 2>&1
+cp zcash-1.0.11/bin/* /usr/local/bin >> $LOG_FILE 2>&1
 zcash-fetch-params >> $LOG_FILE 2>&1
 mkdir ~/.zcash >> $LOG_FILE 2>&1
 mv /tmp/zcash.conf ~/.zcash
