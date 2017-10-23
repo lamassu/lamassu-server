e8474252440117fd054eaafa64d26d19e520c733
Josh Harvey <josh@lamassu.is>
2017-10-23 16:11:25 +0300

    fix up eth 0-conf

---
+++ bin/lamassu-display-config.js
@@ -0,0 +1,12 @@
+const settingsLoader = require('../lib/settings-loader')
+const pp = require('../lib/pp')
+
+settingsLoader.loadLatest()
+.then(r => {
+  pp('config')(r)
+  process.exit(0)
+})
+.catch(e => {
+  console.log(e.stack)
+  process.exit(1)
+})
