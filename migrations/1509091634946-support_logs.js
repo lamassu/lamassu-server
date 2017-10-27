62d606cc80d4a1c78bc3085e4e15e98d99ed01e4
goga-m <goga.mamoulasvili@gmail.com>
2017-10-27 17:36:40 +0300

    Create support_logs (db & api)

---
+++ migrations/1509091634946-support_logs.js
@@ -0,0 +1,17 @@
+var db = require('./db')
+
+exports.up = function (next) {
+  const sql =
+    [`create table support_logs (
+    id uuid PRIMARY KEY,
+    device_id text,
+    timestamp timestamptz not null default now() )`,
+      'alter table logs add column server_timestamp timestamptz not null default now() '
+    ]
+
+  db.multi(sql, next)
+}
+
+exports.down = function (next) {
+  next()
+}
