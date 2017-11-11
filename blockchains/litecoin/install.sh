28979e475c398b521bf7712f254f61f91d15a96f
Neal Conner <neal@lamassu.is>
2017-11-11 18:58:35 -0500

    Update Zcash and Litecoin (#85)
    * Update Zcash to 1.0.12
    
    * Update zcash version in common.js
    
    * Update geth in common.js
    
    * Fix geth dir
    
    * Update litecoin to 0.14.2
    
    * Update litecoin to 0.14.2
    
    * Insert disabledeprecation flag for zcashd
    
    * Add -disabledeprecation=1.0.12
    
    * Remove sed from zcash install.sh

--- blockchains/litecoin/install.sh
+++ blockchains/litecoin/install.sh
@@ -9,9 +9,9 @@ ufw allow 8332 >> $LOG_FILE 2>&1
 ufw allow 22 >> $LOG_FILE 2>&1
 ufw --force enable >> $LOG_FILE 2>&1
 ufw status >> $LOG_FILE 2>&1
-wget -q https://download.litecoin.org/litecoin-0.13.2/linux/litecoin-0.13.2-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
-tar -xzf litecoin-0.13.2-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
-cp litecoin-0.13.2/bin/* /usr/local/bin >> $LOG_FILE 2>&1
+wget -q https://download.litecoin.org/litecoin-0.14.2/linux/litecoin-0.14.2-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
+tar -xzf litecoin-0.14.2-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
+cp litecoin-0.14.2/bin/* /usr/local/bin >> $LOG_FILE 2>&1
 mkdir ~/.litecoin >> $LOG_FILE 2>&1
 mv /tmp/litecoin.conf ~/.litecoin
 mv /tmp/supervisor-litecoin.conf /etc/supervisor/conf.d/litecoin.conf >> $LOG_FILE 2>&1
