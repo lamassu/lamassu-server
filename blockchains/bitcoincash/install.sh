4a83cd7f5a5d8b6b06a07c04bcd391d2d5f40aa6
Neal Conner <neal@lamassu.is>
2017-11-12 10:03:06 -0500

    Custom bin names for bitcoincash

--- blockchains/bitcoincash/install.sh
+++ blockchains/bitcoincash/install.sh
@@ -11,7 +11,8 @@ ufw --force enable >> $LOG_FILE 2>&1
 ufw status >> $LOG_FILE 2>&1
 wget -q https://download.bitcoinabc.org/0.16.0/linux/bitcoin-abc-0.16.0-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
 tar -xzf bitcoin-abc-0.16.0-x86_64-linux-gnu.tar.gz >> $LOG_FILE 2>&1
-cp bitcoin-abc-0.16.0/bin/* /usr/local/bin >> $LOG_FILE 2>&1
+cp bitcoin-abc-0.16.0/bin/bitcoind /usr/local/bin/bitcoincashd >> $LOG_FILE 2>&1
+cp bitcoin-abc-0.16.0/bin/bitcoin-cli /usr/local/bin/bitcoincash-cli >> $LOG_FILE 2>&1
 mkdir ~/.bitcoincash >> $LOG_FILE 2>&1
 mv /tmp/bitcoincash.conf ~/.bitcoincash
 mv /tmp/supervisor-bitcoincash.conf /etc/supervisor/conf.d/bitcoincash.conf >> $LOG_FILE 2>&1
