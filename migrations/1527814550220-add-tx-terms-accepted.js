239ed272935b2e05b656d39125f3656cc9fcd5d9
Fabio Cigliano <fabio.cigliano@gmail.com>
2018-06-02 00:22:55 +1200

    other minor changes

--- migrations/1527814550220-add-tx-terms-accepted.js
+++ migrations/1527814550220-add-tx-terms-accepted.js
@@ -2,8 +2,8 @@ var db = require('./db')
 
 exports.up = function (next) {
   const sql = [
-    'alter table cash_in_txs add column terms_accepted boolean default null',
-    'alter table cash_out_txs add column terms_accepted boolean default null'
+    'alter table cash_in_txs add column terms_accepted boolean not null default false',
+    'alter table cash_out_txs add column terms_accepted boolean not null default false'
   ]
 
   db.multi(sql, next)
