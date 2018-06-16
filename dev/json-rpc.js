eac4f7bb8613c8b843e4a5b8c42076d790091012
Fabio Cigliano <lidio601@users.noreply.github.com>
2018-06-16 22:55:58 +1200

    json-rpc error handling (#128)
    * json-rpc error handling
    
    * partial revert

---
+++ dev/json-rpc.js
@@ -0,0 +1,18 @@
+const rpc = require('../lib/plugins/common/json-rpc')
+
+const account = {
+  username: 'test',
+  password: 'test'
+}
+
+const method = {}
+
+const params = {
+  url: 'https://httpstat.us/500'
+  // url: 'https://httpstat.us/400'
+  // url: 'https://httpstat.us/200'
+}
+
+rpc.fetch(account, method, params)
+  .then(res => console.log('got result', res))
+  .catch(err => console.error('gor error', err))
