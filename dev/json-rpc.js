3c321ac8342f8c266be2b58155bceab7dd5179e0
Fabio Cigliano <lidio601@users.noreply.github.com>
2018-07-02 02:43:58 +1200

    json-rpc parameter bugfix (#129)
    * json-rpc parameter bugfix
    
    * small change
    
    * Update json-rpc.js

--- dev/json-rpc.js
+++ dev/json-rpc.js
@@ -2,13 +2,14 @@ const rpc = require('../lib/plugins/common/json-rpc')
 
 const account = {
   username: 'test',
-  password: 'test'
+  password: 'test',
+  port: 8080
 }
 
 const method = {}
 
 const params = {
-  url: 'https://httpstat.us/500'
+  // url: 'https://httpstat.us/500'
   // url: 'https://httpstat.us/400'
   // url: 'https://httpstat.us/200'
 }
