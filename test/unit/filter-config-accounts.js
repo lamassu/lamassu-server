a3558060373277cdfa7130070414afa656180bd8
Zoran Joka <zoran.joka@gmail.com>
2018-11-08 09:18:49 +0100

    Check for mock codes in test

--- test/unit/filter-config-accounts.js
+++ test/unit/filter-config-accounts.js
@@ -1,3 +1,4 @@
+import _ from 'lodash/fp'
 import test from 'ava'
 
 import { filterAccounts } from '../../lib/admin/config'
@@ -13,12 +14,18 @@ const data = {
 }
 
 test('Do not filter accounts in dev mode', t => {
+  t.plan(3)
   const devMode = true
   const filteredData = filterAccounts(data, devMode)
   t.is(filteredData.accounts.length, 4)
+  t.true(_.some(['code', 'mock-wallet'], filteredData.accounts))
+  t.true(_.some(['code', 'mock-ticker'], filteredData.accounts))
 })
 
 test('Filter accounts in production', t => {
+  t.plan(3)
   const filteredData = filterAccounts(data)
+  t.false(_.some(['code', 'mock-wallet'], filteredData.accounts))
+  t.false(_.some(['code', 'mock-ticker'], filteredData.accounts))
   t.is(filteredData.accounts.length, 2)
 })
