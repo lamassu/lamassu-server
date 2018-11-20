ce46d113691ee30c31185388526ddf6472db26b8
Zoran Joka <zoran.joka@gmail.com>
2018-11-20 21:05:34 +0100

    remove filelds from query, rename migration, delete duplicate migration

--- migrations/1542638179228-alter-machine-pings.js
+++ migrations/1542638179228-alter-machine-pings.js
@@ -1,12 +1,13 @@
 const db = require('./db')
 
 exports.up = function(next) {
-  const sql = [
+  var sql = [
     'ALTER TABLE machine_pings DROP id',
     'ALTER TABLE machine_pings DROP serial_number',
     'ALTER TABLE machine_pings ADD CONSTRAINT PK_device_id PRIMARY KEY (device_id)',
     'ALTER TABLE machine_pings ADD CONSTRAINT U_device_id UNIQUE(device_id)'
   ]
+
   db.multi(sql, next)
 };
 
