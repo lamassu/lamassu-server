6b51075f05fca6126d9e38b6e8df316f8640cdec
Zoran Joka <zoran.joka@gmail.com>
2018-11-21 08:13:07 +0100

    truncate machine_pings in migration

--- migrations/1542638179228-alter-machine-pings.js
+++ migrations/1542638179228-alter-machine-pings.js
@@ -2,6 +2,7 @@ const db = require('./db')
 
 exports.up = function(next) {
   var sql = [
+    'TRUNCATE TABLE machine_pings',
     'ALTER TABLE machine_pings DROP id',
     'ALTER TABLE machine_pings DROP serial_number',
     'ALTER TABLE machine_pings ADD CONSTRAINT PK_device_id PRIMARY KEY (device_id)',
