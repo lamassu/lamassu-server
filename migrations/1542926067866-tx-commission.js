7565c1c2990138c9a277d86ffc8aec4e05aab245
Fabio Cigliano <fabio.cigliano@gmail.com>
2018-11-23 11:42:17 +1300

    migration added

---
+++ migrations/1542926067866-tx-commission.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const db = require('./db')
+
+exports.up = function (next) {
+  var sql = [
+    'ALTER TABLE cash_in_txs ADD COLUMN commission_percentage numeric(14, 5) null DEFAULT null',
+    'ALTER TABLE cash_out_txs ADD COLUMN commission_percentage numeric(14, 5) null DEFAULT null'
+  ]
+
+  db.multi(sql, next)
+}
+
+exports.down = function (next) {
+  next()
+}
