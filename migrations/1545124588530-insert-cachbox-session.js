39a41f8cbb48d721468ff489c79074c5912c5f20
Davit Abulashvili <datobula@gmail.com>
2019-01-08 12:22:06 +0400

    create index on device_time column on cashbox_sessions

--- migrations/1545124588530-insert-cachbox-session.js
+++ migrations/1545124588530-insert-cachbox-session.js
@@ -1,11 +1,15 @@
 var db = require('./db')
 
 exports.up = function (next) {
-  db.multi(['CREATE TABLE IF NOT EXISTS cashbox_sessions ( ' +
-  'id uuid PRIMARY KEY, ' +
-  'device_id text NOT NULL, ' +
-  'device_time bigint NOT NULL, ' +
-  'created timestamp NOT NULL DEFAULT now() )'], next)
+  db.multi([
+    'CREATE TABLE IF NOT EXISTS cashbox_sessions ( ' +
+    'id uuid PRIMARY KEY, ' +
+    'device_id text NOT NULL, ' +
+    'device_time bigint NOT NULL, ' +
+    'created timestamp NOT NULL DEFAULT now() )',
+
+    'CREATE INDEX ON cashbox_sessions (device_time)'
+  ], next)
 }
 
 exports.down = function (next) {
