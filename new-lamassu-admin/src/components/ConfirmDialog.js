d0e24b1a74b5aded02ae4f0e1e7e653e17071574
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-12-17 01:19:35 -0600

    style: minor style fixes

--- new-lamassu-admin/src/components/ConfirmDialog.js
+++ new-lamassu-admin/src/components/ConfirmDialog.js
@@ -1,16 +1,17 @@
-import React, { memo } from 'react'
-import { DialogActions, DialogContent, DialogContentText, IconButton, Dialog, DialogTitle as MuiDialogTitle } from '@material-ui/core'
+import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle as MuiDialogTitle, IconButton } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
+import React, { memo } from 'react'
 import { Button } from '../components/buttons'
+import { ReactComponent as CloseIcon } from '../styling/icons/action/close/zodiac.svg'
+import { spacer } from '../styling/variables'
 import { TextInput } from './inputs'
 import { H4, P } from './typography'
-import { ReactComponent as CloseIcon } from '../styling/icons/action/close/zodiac.svg'
 
-const styles = theme => ({
+const styles = () => ({
   closeButton: {
     position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1)
+    right: spacer,
+    top: spacer
   }
 })
 
@@ -19,19 +20,17 @@ const DialogTitle = withStyles(styles)(props => {
   return (
     <MuiDialogTitle>
       {children}
-
-      {onClose ? (
+      {onClose &&
         <IconButton aria-label='close' className={classes.closeButton} onClick={onClose}>
           <CloseIcon />
-        </IconButton>
-      ) : null}
+        </IconButton>}
     </MuiDialogTitle>
   )
 })
 
 const ConfirmDialog = memo(({ title = 'Confirm action', subtitle = 'This action requires confirmation', open, toBeConfirmed, onConfirmed, onDissmised, ...props }) => {
   const [value, setValue] = React.useState('')
-  const [_open, setOpen] = React.useState(open)
+  const [isOpen, setOpen] = React.useState(open)
 
   const handleChange = event => {
     setValue(event.target.value)
@@ -48,7 +47,7 @@ const ConfirmDialog = memo(({ title = 'Confirm action', subtitle = 'This action 
   }
 
   return (
-    <Dialog open={_open} aria-labelledby='form-dialog-title' {...props}>
+    <Dialog open={isOpen} aria-labelledby='form-dialog-title' {...props}>
       <DialogTitle id='customized-dialog-title' onClose={handleDissmised}>
         <H4>{title}</H4>
         {subtitle && <DialogContentText><P>{subtitle}</P></DialogContentText>}
