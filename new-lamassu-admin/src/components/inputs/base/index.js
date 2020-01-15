fbe5238660e0fb591e608bb752f6636b463ebb17
Luis Félix <5789328+lcsfelix@users.noreply.github.com>
2020-01-15 22:41:47 +0000

    feat: add operator info page
    feat: save/load operator info
    
    feat: add formik switch component
    
    feat: add input validation
    
    fix: correct formik switch behaviour
    
    fix: change infoCardEnabled to a radio input
    
    style: move styles out of js
    
    feat: add error feedback

--- new-lamassu-admin/src/components/inputs/base/index.js
+++ new-lamassu-admin/src/components/inputs/base/index.js
@@ -1,4 +1,6 @@
 import Checkbox from './Checkbox'
 import TextInput from './TextInput'
+import Switch from './Switch'
+import RadioGroup from './RadioGroup'
 
-export { Checkbox, TextInput }
+export { Checkbox, TextInput, Switch, RadioGroup }
---
+++ new-lamassu-admin/src/components/inputs/formik/RadioGroup.js
@@ -0,0 +1,42 @@
+import React, { memo } from 'react'
+import { makeStyles } from '@material-ui/core'
+
+import { Label1 } from 'src/components/typography'
+
+import { RadioGroup } from '../base'
+
+const styles = {
+  label: {
+    height: 16,
+    lineHeight: '16px',
+    margin: [[0, 0, 4, 0]]
+  }
+}
+
+const useStyles = makeStyles(styles)
+
+const RadioGroupFormik = memo(({ ...props }) => {
+  const classes = useStyles()
+
+  const { name, onChange, value } = props.field
+
+  return (
+    <>
+      {props.label && <Label1 className={classes.label}>{props.label}</Label1>}
+      <RadioGroup
+        name={name}
+        value={value}
+        options={props.options}
+        ariaLabel={name}
+        onChange={e => {
+          onChange(e)
+          props.resetError()
+        }}
+        className={props.className}
+        {...props}
+      />
+    </>
+  )
+})
+
+export default RadioGroupFormik
