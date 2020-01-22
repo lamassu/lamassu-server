c7bb321d8275e2cd3e509c97e94cdddf20b3d6d6
Luis Félix <5789328+lcsfelix@users.noreply.github.com>
2020-01-22 12:52:50 +0000

    feat: add terms and conditions page
    feat: add modal preview
    
    feat: remove preview
    
    fix: increase space between switch and fields

--- new-lamassu-admin/src/pages/OperatorInfo/OperatorInfo.js
+++ new-lamassu-admin/src/pages/OperatorInfo/OperatorInfo.js
@@ -9,6 +9,7 @@ import logsStyles from '../Logs.styles'
 
 import CoinAtmRadar from './CoinATMRadar'
 import ContactInfo from './ContactInfo'
+import TermsConditions from './TermsConditions'
 
 const localStyles = {
   contentWrapper: {
@@ -50,6 +51,7 @@ const OperatorInfo = () => {
         />
         <div className={classes.contentWrapper}>
           {isSelected(CONTACT_INFORMATION) && <ContactInfo />}
+          {isSelected(TERMS_CONDITIONS) && <TermsConditions />}
           {isSelected(COIN_ATM_RADAR) && <CoinAtmRadar />}
         </div>
       </div>
