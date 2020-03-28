a28a5b919a98028ca514dd25dc22b2115e6e53b4
Liordino Neto <liordinoneto@gmail.com>
2020-03-28 19:38:25 -0300

    feat: created the base skeleton for the compliance triggers page

---
+++ new-lamassu-admin/src/pages/Triggers/Triggers.styles.js
@@ -0,0 +1,10 @@
+import baseStyles from 'src/pages/Logs.styles'
+
+const { titleWrapper, titleAndButtonsContainer } = baseStyles
+
+const mainStyles = {
+  titleWrapper,
+  titleAndButtonsContainer
+}
+
+export { mainStyles }
