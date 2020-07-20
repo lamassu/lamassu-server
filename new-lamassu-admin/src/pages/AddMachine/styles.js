919af92f6c6c5fe693f4f169b0ab1ad26486e21f
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2020-07-19 20:54:23 -0500

    feat: close add machine wizard on success
    fix: remove done button
    
    fix: auto close on success
    
    fix: show success message
    
    fix: move add machine to main nav
    
    fix: steps overlap
    
    fix: no console logs, simplify some lines
    
    fix: remove spinner and toast
    
    fix: show info3 message on pairing success
    
    fix: eslint
    
    fix: consider the machine name while verifying if the new machine is
    paired

--- new-lamassu-admin/src/pages/AddMachine/styles.js
+++ new-lamassu-admin/src/pages/AddMachine/styles.js
@@ -1,4 +1,12 @@
-import { backgroundColor, mainWidth } from 'src/styling/variables'
+import typographyStyles from 'src/components/typography/styles'
+import {
+  placeholderColor,
+  backgroundColor,
+  primaryColor,
+  mainWidth
+} from 'src/styling/variables'
+
+const { tl2, p } = typographyStyles
 
 const fill = '100%'
 const flexDirection = 'column'
@@ -50,13 +58,43 @@ const styles = {
   qrTextWrapper: {
     width: 381,
     marginLeft: 80,
-    display: 'flex'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   qrTextIcon: {
     marginRight: 16
   },
   qrText: {
     marginTop: 0
+  },
+  item: {
+    position: 'relative',
+    margin: '12px 0 12px 0',
+    display: 'flex'
+  },
+  itemText: {
+    extend: p,
+    color: placeholderColor,
+    marginRight: 24
+  },
+  itemTextActive: {
+    extend: tl2,
+    color: primaryColor
+  },
+  itemTextPast: {
+    color: primaryColor
+  },
+  stepperPath: {
+    position: 'absolute',
+    height: 25,
+    width: 1,
+    border: [[1, 'solid', placeholderColor]],
+    right: 8,
+    top: 18
+  },
+  stepperPast: {
+    border: [[1, 'solid', primaryColor]]
   }
 }
 
