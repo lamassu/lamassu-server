0530a994fd8a250ff22d1e3a6b4e6a26b509b1aa
Liordino Neto <liordinoneto@gmail.com>
2020-08-25 22:47:07 -0300

    fix: make the schema_version column not nullable

--- migrations/1595295132361-schema-version-on-user-config.js
+++ migrations/1595295132361-schema-version-on-user-config.js
@@ -2,7 +2,7 @@ const db = require('./db')
 
 module.exports.up = function (next) {
   var sql = [
-    'alter table user_config add column schema_version smallint DEFAULT 1'
+    'alter table user_config add column schema_version smallint not null DEFAULT 1'
   ]
 
   db.multi(sql, next)
