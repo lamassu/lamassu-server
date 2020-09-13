7fbdc04b38628956bda4a0236213d52e1b57ddb4
Liordino Neto <liordinoneto@gmail.com>
2020-09-13 15:20:22 -0300

    feat: create migration to move machine names from file to db
    fix: updates machine names before the config migration
    
    fix: load machineLoader
    
    fix: create a param to ignore the schema version when loading the latest
    config using the old loader

--- migrations/1599523522436-migrate-config.js
+++ migrations/1599523522436-migrate-config.js
@@ -1,16 +1,36 @@
+const db = require('./db')
 const settingsLoader = require('../lib/settings-loader')
+const machineLoader = require('../lib/machine-loader')
 const { saveConfig, saveAccounts } = require('../lib/new-settings-loader')
 const { migrate } = require('../lib/config-migration')
 
 module.exports.up = function (next) {
-  settingsLoader.loadLatest()
-    .then(settings => migrate(settings.config, settings.accounts))
-    .then(newSettings => Promise.all([
-      saveConfig(newSettings.config),
-      saveAccounts(newSettings.accounts)
-    ]))
-    .then(() => next())
-    .catch(err => console.error(err))
+  function migrateConfig(settings) {
+    return migrate(settings.config, settings.accounts)
+      .then(newSettings => Promise.all([
+        saveConfig(newSettings.config),
+        saveAccounts(newSettings.accounts)
+      ]))
+      .then(() => next())
+  }
+
+  settingsLoader.loadLatest(false)
+    .then(async settings => ({
+      settings,
+      machines: await machineLoader.getMachineNames(settings.config)
+    }))
+    .then(({ settings, machines }) => {
+      const sql = machines 
+        ? machines.map(m => `update devices set name = '${m.name}' where device_id = '${m.deviceId}'`)
+        : []
+      return db.multi(sql, () => migrateConfig(settings))
+    })
+    .catch(err => {
+      if (err.message = 'lamassu-server is not configured')
+        next()
+      
+      console.log(err.message)
+    })
 }
 
 module.exports.down = function (next) {
