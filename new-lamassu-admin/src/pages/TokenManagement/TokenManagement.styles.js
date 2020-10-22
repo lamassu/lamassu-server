7e6561c5a9b12c7797b3ca87fd2f7aca004ea412
Sérgio Salgado <sergiodiassalgado@gmail.com>
2020-10-22 11:24:00 +0100

    feat: add revoke token button

--- new-lamassu-admin/src/pages/TokenManagement/TokenManagement.styles.js
+++ new-lamassu-admin/src/pages/TokenManagement/TokenManagement.styles.js
@@ -1,33 +1,10 @@
-import typographyStyles from 'src/components/typography/styles'
 import baseStyles from 'src/pages/Logs.styles'
 
-const { label1 } = typographyStyles
-const { titleWrapper, titleAndButtonsContainer, buttonsWrapper } = baseStyles
+const { titleWrapper, titleAndButtonsContainer } = baseStyles
 
 const mainStyles = {
   titleWrapper,
-  titleAndButtonsContainer,
-  buttonsWrapper,
-  headerLabels: {
-    display: 'flex',
-    flexDirection: 'row',
-    '& div': {
-      display: 'flex',
-      alignItems: 'center'
-    },
-    '& > div:first-child': {
-      marginRight: 24
-    },
-    '& span': {
-      extend: label1,
-      marginLeft: 6
-    }
-  },
-  overflowTd: {
-    overflow: 'hidden',
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis'
-  }
+  titleAndButtonsContainer
 }
 
 export { mainStyles }
