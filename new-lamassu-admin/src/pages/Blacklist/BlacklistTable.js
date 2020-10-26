aa0a97318354017da03356093737dc4adaed6f62
Cesar <a79014@alunos.uminho.pt>
2020-10-26 19:25:08 +0000

    fix: Brings unrelated logic outside of BlacklistTable.js

--- new-lamassu-admin/src/pages/Blacklist/BlacklistTable.js
+++ new-lamassu-admin/src/pages/Blacklist/BlacklistTable.js
@@ -1,35 +1,18 @@
-import { Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import * as R from 'ramda'
 import React from 'react'
 
-import Tooltip from 'src/components/Tooltip'
 import { IconButton } from 'src/components/buttons'
-import { Switch } from 'src/components/inputs'
 import DataTable from 'src/components/tables/DataTable'
-import { H4, Label1, Label2, P } from 'src/components/typography'
+import { Label1 } from 'src/components/typography'
 import CopyToClipboard from 'src/pages/Transactions/CopyToClipboard'
 import { ReactComponent as DeleteIcon } from 'src/styling/icons/action/delete/enabled.svg'
-import { fromNamespace, toNamespace } from 'src/utils/config'
 
 import styles from './Blacklist.styles'
+
 const useStyles = makeStyles(styles)
 
-const BlacklistTable = ({
-  data,
-  selectedCoin,
-  handleDeleteEntry,
-  saveConfig,
-  configData
-}) => {
-  const complianceConfig =
-    configData?.config && fromNamespace('compliance')(configData.config)
-  const rejectAddressReuse = complianceConfig?.rejectAddressReuse ?? false
-  const addressReuseSave = rawConfig => {
-    const config = toNamespace('compliance')(rawConfig)
-    return saveConfig({ variables: { config } })
-  }
-
+const BlacklistTable = ({ data, selectedCoin, handleDeleteEntry }) => {
   const classes = useStyles()
 
   const elements = [
@@ -71,31 +54,6 @@ const BlacklistTable = ({
 
   return (
     <>
-      <Box display="flex" justifyContent="space-between" mb={3}>
-        <H4 noMargin className={classes.subtitle}>
-          {selectedCoin.display
-            ? `${selectedCoin.display} blacklisted addresses`
-            : ''}{' '}
-        </H4>
-        <Box display="flex" alignItems="center" justifyContent="end" mr="-5px">
-          <P>Reject reused addresses</P>
-          <Switch
-            checked={rejectAddressReuse}
-            onChange={event => {
-              addressReuseSave({ rejectAddressReuse: event.target.checked })
-            }}
-            value={rejectAddressReuse}
-          />
-          <Label2>{rejectAddressReuse ? 'On' : 'Off'}</Label2>
-          <Tooltip width={304}>
-            <P>
-              The "Reject reused addresses" option means that all addresses that
-              are used once will be automatically rejected if there's an attempt
-              to use them again on a new transaction.
-            </P>
-          </Tooltip>
-        </Box>
-      </Box>
       <DataTable data={dataToShow} elements={elements} name="blacklistTable" />
     </>
   )
