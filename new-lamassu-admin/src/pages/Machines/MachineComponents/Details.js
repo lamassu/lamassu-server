2161337fd78265b4a722df6a1bdbe9b71927af09
Cesar <a79014@alunos.uminho.pt>
2020-10-29 23:00:49 +0000

    feat: Cash cassettes info in machine dashboard

--- new-lamassu-admin/src/pages/Machines/MachineComponents/Details.js
+++ new-lamassu-admin/src/pages/Machines/MachineComponents/Details.js
@@ -1,7 +1,11 @@
+import { useMutation } from '@apollo/react-hooks'
 import { makeStyles } from '@material-ui/core/styles'
+import gql from 'graphql-tag'
 import moment from 'moment'
-import React from 'react'
+import React, { useState } from 'react'
 
+import { ConfirmDialog } from 'src/components/ConfirmDialog'
+import { Status } from 'src/components/Status'
 import ActionButton from 'src/components/buttons/ActionButton'
 import { Label4, P } from 'src/components/typography'
 import { ReactComponent as RebootReversedIcon } from 'src/styling/icons/button/reboot/white.svg'
@@ -13,9 +17,36 @@ import { ReactComponent as UnpairIcon } from 'src/styling/icons/button/unpair/zo
 
 import styles from '../Machines.styles'
 const useStyles = makeStyles(styles)
-const Details = ({ data }) => {
+
+const MACHINE_ACTION = gql`
+  mutation MachineAction(
+    $deviceId: ID!
+    $action: MachineAction!
+    $newName: String
+  ) {
+    machineAction(deviceId: $deviceId, action: $action, newName: $newName) {
+      deviceId
+    }
+  }
+`
+
+const Details = ({ data, onActionSuccess }) => {
+  const [action, setAction] = useState('')
+  const [confirmActionDialogOpen, setConfirmActionDialogOpen] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [machineAction] = useMutation(MACHINE_ACTION, {
+    onError: ({ message }) => {
+      const errorMessage = message ?? 'An error ocurred'
+      setErrorMessage(errorMessage)
+    },
+    onCompleted: () => {
+      onActionSuccess && onActionSuccess()
+      setConfirmActionDialogOpen(false)
+    }
+  })
+  const confirmActionDialog = action =>
+    setAction(action) || setConfirmActionDialogOpen(true)
   const classes = useStyles()
-  console.log(data)
   return (
     <>
       <div
@@ -24,44 +55,6 @@ const Details = ({ data }) => {
           flexDirection: 'row',
           justifyContent: 'space-around'
         }}>
-        <div style={{ flex: 1 }}>
-          <Label4 className={classes.tl2}>Paired at</Label4>
-          <P>
-            {data.pairedAt
-              ? moment(data.pairedAt).format('YYYY-MM-DD HH:mm:ss')
-              : ''}
-          </P>
-        </div>
-        <div style={{ flex: 1 }}>
-          {' '}
-          <Label4 className={classes.tl2}>Machine model</Label4>
-          <P>{data.model}</P>
-        </div>
-        <div style={{ flex: 1 }}>
-          {' '}
-          <Label4 className={classes.tl2}>Software version</Label4>
-          <P>{data.version}</P>
-        </div>
-      </div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-around'
-        }}>
-        <div style={{ flex: 1 }}>
-          <Label4 className={classes.tl2}>Status</Label4>
-          <P>{data && data.statuses ? data.statuses[0].label : ''}</P>
-        </div>
-        <div style={{ flex: 1 }}>
-          {' '}
-          <Label4 className={classes.tl2}>Last ping</Label4>
-          <P>
-            {data.lastPing
-              ? moment(data.lastPing).format('YYYY-MM-DD HH:mm:ss')
-              : ''}
-          </P>
-        </div>
         <div style={{ flex: 1 }}>
           {' '}
           <Label4 className={classes.tl2}>Actions</Label4>
@@ -77,8 +70,7 @@ const Details = ({ data }) => {
                 className={classes.mr}
                 Icon={UnpairIcon}
                 InverseIcon={UnpairReversedIcon}
-                disabled={() => false}
-                onClick={it => console.log(it)}>
+                onClick={() => confirmActionDialog('Unpair')}>
                 Unpair
               </ActionButton>
               <ActionButton
@@ -87,8 +79,7 @@ const Details = ({ data }) => {
                 className={classes.mr}
                 Icon={RebootIcon}
                 InverseIcon={RebootReversedIcon}
-                disabled={() => false}
-                onClick={it => console.log(it)}>
+                onClick={() => confirmActionDialog('Reboot')}>
                 Reboot
               </ActionButton>
               <ActionButton
@@ -97,14 +88,74 @@ const Details = ({ data }) => {
                 color="primary"
                 Icon={ShutdownIcon}
                 InverseIcon={ShutdownReversedIcon}
-                disabled={() => false}
-                onClick={it => console.log(it)}>
+                onClick={() => confirmActionDialog('Shutdown')}>
                 Shutdown
               </ActionButton>
             </div>
           )}
         </div>
+        <div style={{ flex: 1 }}>
+          {' '}
+          <Label4 className={classes.tl2}>Status</Label4>
+          {data && data.statuses ? <Status status={data.statuses[0]} /> : null}
+          {/*           <Label4 className={classes.tl2}>Machine model</Label4>
+          <P>{data.model}</P> */}
+        </div>
+        <div style={{ flex: 1 }}>
+          {' '}
+          <Label4 className={classes.tl2}>Last ping</Label4>
+          <P>
+            {data.lastPing
+              ? moment(data.lastPing).format('YYYY-MM-DD HH:mm:ss')
+              : ''}
+          </P>
+        </div>
       </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-around'
+        }}>
+        <div style={{ flex: 1 }}>
+          <Label4 className={classes.tl2}>Machine model</Label4>
+          <P>{data.model}</P>
+        </div>
+        <div style={{ flex: 1 }}>
+          {' '}
+          <Label4 className={classes.tl2}>Software version</Label4>
+          <P>{data.version}</P>
+        </div>
+
+        <div style={{ flex: 1 }}>
+          {' '}
+          <Label4 className={classes.tl2}>Paired at</Label4>
+          <P>
+            {data.pairedAt
+              ? moment(data.pairedAt).format('YYYY-MM-DD HH:mm:ss')
+              : ''}
+          </P>
+        </div>
+      </div>
+      <ConfirmDialog
+        open={confirmActionDialogOpen}
+        title={`${action} this machine?`}
+        errorMessage={errorMessage}
+        toBeConfirmed={data.name}
+        onConfirmed={() => {
+          setErrorMessage(null)
+          machineAction({
+            variables: {
+              deviceId: data.deviceId,
+              action: `${action}`.toLowerCase()
+            }
+          })
+        }}
+        onDissmised={() => {
+          setConfirmActionDialogOpen(false)
+          setErrorMessage(null)
+        }}
+      />
     </>
   )
 }
