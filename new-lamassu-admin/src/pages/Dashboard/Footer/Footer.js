311d8e6e06558e5fce4bf6e1961bae71b719eb23
Cesar <a79014@alunos.uminho.pt>
2020-11-05 17:05:22 +0000

    feat: Footer coin styling

--- new-lamassu-admin/src/pages/Dashboard/Footer/Footer.js
+++ new-lamassu-admin/src/pages/Dashboard/Footer/Footer.js
@@ -1,30 +1,28 @@
 import { useQuery } from '@apollo/react-hooks'
 import { makeStyles } from '@material-ui/core'
+import Grid from '@material-ui/core/Grid'
 import gql from 'graphql-tag'
+import * as R from 'ramda'
 import React from 'react'
 
-import { white } from 'src/styling/variables'
+import { Label2 } from 'src/components/typography'
+import { ReactComponent as TxInIcon } from 'src/styling/icons/direction/cash-in.svg'
+import { ReactComponent as TxOutIcon } from 'src/styling/icons/direction/cash-out.svg'
+
+import styles from './Footer.styles'
 
 const GET_DATA = gql`
   query getData {
     rates
+    cryptoCurrencies {
+      code
+      display
+    }
+    config
   }
 `
 
-const useStyles = makeStyles({
-  footer: {
-    position: 'fixed',
-    left: 0,
-    bottom: 0,
-    width: '100vw',
-    backgroundColor: white,
-    textAlign: 'left'
-  },
-  content: {
-    width: 1200,
-    margin: '0 auto'
-  }
-})
+const useStyles = makeStyles(styles)
 const Footer = () => {
   const { data, loading } = useQuery(GET_DATA)
 
@@ -32,11 +30,37 @@ const Footer = () => {
 
   console.log(data, loading)
 
+  const renderFooterItem = key => {
+    const idx = R.findIndex(R.propEq('code', key))(data.cryptoCurrencies)
+    return (
+      <Grid key={key} item xs={3} style={{ marginBottom: 18 }}>
+        <Label2 className={classes.label}>
+          {data.cryptoCurrencies[idx].display}
+        </Label2>
+        <div className={classes.headerLabels}>
+          <div>
+            <TxInIcon />
+            <span>{`${data.rates[key].cashIn} ${data.config.locale_fiatCurrency}`}</span>
+          </div>
+          <div>
+            <TxOutIcon />
+            <span>{`${data.rates[key].cashOut} ${data.config.locale_fiatCurrency}`}</span>
+          </div>
+        </div>
+      </Grid>
+    )
+  }
   return (
     <>
       <div className={classes.footer}>
         <div className={classes.content}>
-          <h1>Footer content</h1>
+          {!loading && (
+            <>
+              <Grid container spacing={1}>
+                {R.keys(data.rates).map(key => renderFooterItem(key))}
+              </Grid>
+            </>
+          )}
         </div>
       </div>
     </>
