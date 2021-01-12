393b4b8cdda7a25dd2088ff8b002cfe4912f9cab
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-01-12 18:17:32 +0000

    feat: idle machine polling

---
+++ test/stress/utils/variables.js
@@ -0,0 +1,5 @@
+const NUMBER_OF_MACHINES = 4
+const SERVER_CERT_PATH = `../../certs/Lamassu_OP_Root_CA.pem`
+const MACHINE_PATH = `../../../lamassu-machine`
+
+module.exports = { NUMBER_OF_MACHINES, SERVER_CERT_PATH, MACHINE_PATH }
