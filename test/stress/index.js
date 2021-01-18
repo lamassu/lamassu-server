dbb2e1b5f9e4c9c3743848ba1f92797b9e72237a
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-01-18 16:03:28 +0000

    fix: remove unwanted async calls

--- test/stress/index.js
+++ test/stress/index.js
@@ -5,8 +5,8 @@ const minimist = require('minimist')
 const cmd = require('./scripts')
 const variables = require('./utils/variables')
 
-async function createMachines (numberOfMachines) {
-  await cmd.execCommand(
+function createMachines (numberOfMachines) {
+  return cmd.execCommand(
     `bash ./scripts/create-machines.sh ${numberOfMachines} ${variables.SERVER_CERT_PATH} ${variables.MACHINE_PATH}`
   )
 }
@@ -20,8 +20,8 @@ async function run (args = minimist(process.argv.slice(2))) {
   const NUMBER_OF_MACHINES = args._[0]
   const HAS_VARIANCE = args.v || false
 
-  startServer()
   await createMachines(NUMBER_OF_MACHINES)
+  startServer()
 
   for (let i = 1; i <= NUMBER_OF_MACHINES; i++) {
     const forked = fork('child.js')
