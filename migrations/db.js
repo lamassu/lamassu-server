714239ff84b962e8d5b34eafe53e89d31bcae8ed
csrapr <26280794+csrapr@users.noreply.github.com>
2021-03-02 17:33:47 +0000

    Feat: make lamassu-migrate use async local storage

--- migrations/db.js
+++ migrations/db.js
@@ -1,7 +1,7 @@
 const db = require('../lib/db')
 const sequential = require('promise-sequential')
 
-module.exports = {multi}
+module.exports = { multi }
 
 function multi (sqls, cb) {
   const doQuery = s => {
