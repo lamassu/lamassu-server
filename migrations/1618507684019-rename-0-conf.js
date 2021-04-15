e86a061f984476e5cb3d4f50ebc9701638b71160
csrapr <26280794+csrapr@users.noreply.github.com>
2021-04-15 18:56:31 +0100

    Feat: migration rename non blockcypher to none

---
+++ migrations/1618507684019-rename-0-conf.js
@@ -0,0 +1,21 @@
+const _ = require('lodash/fp')
+const settingsLoader = require('../lib/new-settings-loader')
+const configManager = require('../lib/new-config-manager')
+
+exports.up = async function (next) {
+  const { config } = await settingsLoader.loadLatest()
+  const cryptoCodes = configManager.getCryptosFromWalletNamespace(config)
+  _.forEach(cryptoCode => {
+    const key = `wallets_${cryptoCode}_zeroConf`
+    const zeroConfSetting = _.get(key, config)
+    if (cryptoCode === 'BTC' && zeroConfSetting === 'blockcypher') return
+    if (!_.isNil(zeroConfSetting) && zeroConfSetting !== 'none') {
+      config[key] = 'none'
+    }
+  }, cryptoCodes)
+  return settingsLoader.saveConfig(config).then(() => next()).catch(err => next(err))
+}
+
+exports.down = function (next) {
+  next()
+}
