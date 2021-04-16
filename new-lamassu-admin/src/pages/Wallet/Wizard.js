c3225c9efa0114d39d4bb4bfdfedc4139fc87d98
csrapr <26280794+csrapr@users.noreply.github.com>
2021-04-16 16:04:34 +0100

    Chore: remove 1-conf check in wallet plugin
    Chore: Use _.isNil

--- new-lamassu-admin/src/pages/Wallet/Wizard.js
+++ new-lamassu-admin/src/pages/Wallet/Wizard.js
@@ -50,7 +50,6 @@ const Wizard = ({ coin, onClose, accountsConfig, accounts, save, error }) => {
 
   const getValue = code => R.find(R.propEq('code', code))(accounts)
 
-  const limit = zeroConfLimit && coin.code !== 'ETH' ? zeroConfLimit : 0
   const onContinue = async (stepConfig, stepAccount) => {
     const newConfig = R.merge(config, stepConfig)
     const newAccounts = stepAccount
@@ -58,8 +57,8 @@ const Wizard = ({ coin, onClose, accountsConfig, accounts, save, error }) => {
       : accountsToSave
 
     if (isLastStep) {
-      newConfig.zeroConfLimit = limit
-      return save(toNamespace(coin.code, newConfig), newAccounts)
+      const configToSave = { ...newConfig, zeroConfLimit: 0 }
+      return save(toNamespace(coin.code, configToSave), newAccounts)
     }
 
     setState({
