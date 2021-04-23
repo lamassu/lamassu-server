94b2b7a1e2c38c5327b8d10aa3f5105f4936ad28
João Leal <37.joao.leal@gmail.com>
2021-04-23 15:01:39 +0100

    fix: add machine actions to 'Dashboard > Machine Details'

--- new-lamassu-admin/src/pages/Machines/MachineComponents/Overview.js
+++ new-lamassu-admin/src/pages/Machines/MachineComponents/Overview.js
@@ -1,34 +1,13 @@
-import { useMutation } from '@apollo/react-hooks'
 import { makeStyles } from '@material-ui/core/styles'
-import gql from 'graphql-tag'
 import moment from 'moment'
-import React, { useState } from 'react'
+import React from 'react'
 
-import { ConfirmDialog } from 'src/components/ConfirmDialog'
 import { Status } from 'src/components/Status'
-import ActionButton from 'src/components/buttons/ActionButton'
+import MachineActions from 'src/components/machineActions/MachineActions'
 import { H3, Label3, P } from 'src/components/typography'
-import { ReactComponent as RebootReversedIcon } from 'src/styling/icons/button/reboot/white.svg'
-import { ReactComponent as RebootIcon } from 'src/styling/icons/button/reboot/zodiac.svg'
-import { ReactComponent as ShutdownReversedIcon } from 'src/styling/icons/button/shut down/white.svg'
-import { ReactComponent as ShutdownIcon } from 'src/styling/icons/button/shut down/zodiac.svg'
-import { ReactComponent as UnpairReversedIcon } from 'src/styling/icons/button/unpair/white.svg'
-import { ReactComponent as UnpairIcon } from 'src/styling/icons/button/unpair/zodiac.svg'
 
 import styles from '../Machines.styles'
 const useStyles = makeStyles(styles)
-
-const MACHINE_ACTION = gql`
-  mutation MachineAction(
-    $deviceId: ID!
-    $action: MachineAction!
-    $newName: String
-  ) {
-    machineAction(deviceId: $deviceId, action: $action, newName: $newName) {
-      deviceId
-    }
-  }
-`
 
 const makeLastPing = lastPing => {
   if (!lastPing) return null
@@ -50,24 +29,7 @@ const makeLastPing = lastPing => {
 }
 
 const Overview = ({ data, onActionSuccess }) => {
-  const [action, setAction] = useState('')
-  const [confirmActionDialogOpen, setConfirmActionDialogOpen] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
   const classes = useStyles()
-
-  const [machineAction] = useMutation(MACHINE_ACTION, {
-    onError: ({ message }) => {
-      const errorMessage = message ?? 'An error ocurred'
-      setErrorMessage(errorMessage)
-    },
-    onCompleted: () => {
-      onActionSuccess && onActionSuccess()
-      setConfirmActionDialogOpen(false)
-    }
-  })
-
-  const confirmActionDialog = action =>
-    setAction(action) || setConfirmActionDialogOpen(true)
 
   return (
     <>
@@ -90,57 +52,11 @@ const Overview = ({ data, onActionSuccess }) => {
       </div>
       <div className={classes.row}>
         <div className={classes.rowItem}>
-          {' '}
-          <Label3 className={classes.label3}>Actions</Label3>
-          {data.name && (
-            <div className={classes.actionButtonsContainer}>
-              <ActionButton
-                color="primary"
-                className={classes.actionButton}
-                Icon={UnpairIcon}
-                InverseIcon={UnpairReversedIcon}
-                onClick={() => confirmActionDialog('Unpair')}>
-                Unpair
-              </ActionButton>
-              <ActionButton
-                color="primary"
-                className={classes.actionButton}
-                Icon={RebootIcon}
-                InverseIcon={RebootReversedIcon}
-                onClick={() => confirmActionDialog('Reboot')}>
-                Reboot
-              </ActionButton>
-              <ActionButton
-                className={classes.actionButton}
-                color="primary"
-                Icon={ShutdownIcon}
-                InverseIcon={ShutdownReversedIcon}
-                onClick={() => confirmActionDialog('Shutdown')}>
-                Shutdown
-              </ActionButton>
-            </div>
-          )}
+          <MachineActions
+            machine={data}
+            onActionSuccess={onActionSuccess}></MachineActions>
         </div>
       </div>
-      <ConfirmDialog
-        open={confirmActionDialogOpen}
-        title={`${action} this machine?`}
-        errorMessage={errorMessage}
-        toBeConfirmed={data.name}
-        onConfirmed={() => {
-          setErrorMessage(null)
-          machineAction({
-            variables: {
-              deviceId: data.deviceId,
-              action: `${action}`.toLowerCase()
-            }
-          })
-        }}
-        onDissmised={() => {
-          setConfirmActionDialogOpen(false)
-          setErrorMessage(null)
-        }}
-      />
     </>
   )
 }
