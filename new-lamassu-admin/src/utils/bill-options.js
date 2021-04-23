ae4672ee8c6d4e794cf8b3fda82d4778f189f0f0
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-04-23 11:39:47 +0100

    fix: bill denomination not showing on edit

--- new-lamassu-admin/src/utils/bill-options.js
+++ new-lamassu-admin/src/utils/bill-options.js
@@ -3,7 +3,7 @@ import * as R from 'ramda'
 const getBillOptions = R.curry((locale, denomiations) => {
   const currency = R.prop('fiatCurrency')(locale)
   return R.compose(
-    R.map(code => ({ code, display: code })),
+    R.map(code => ({ code: parseInt(code), display: code })),
     R.keys,
     R.path([currency])
   )(denomiations)
