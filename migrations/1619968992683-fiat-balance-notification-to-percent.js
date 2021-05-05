126a690c591c50763f7217549dfeeb056b4a388f
José Oliveira <josepfo.w@gmail.com>
2021-05-05 16:52:29 +0100

    refactor: insert only new fields in user config

--- migrations/1619968992683-fiat-balance-notification-to-percent.js
+++ migrations/1619968992683-fiat-balance-notification-to-percent.js
@@ -1,43 +1,40 @@
 const _ = require('lodash/fp')
 const { saveConfig, loadLatest } = require('../lib/new-settings-loader')
+const { cassetteMaxCapacity } = require('../lib/constants')
 
 exports.up = function (next) {
-  loadLatest()
+  return loadLatest()
     .then(({ config }) => {
       const fiatBalance1 = config.notifications_fiatBalanceCassette1
       const fiatBalance2 = config.notifications_fiatBalanceCassette2
-
+      const overrides = config.notifications_fiatBalanceOverrides
+      const newConfig = {}
       if (fiatBalance1) {
-        config.notifications_fiatBalanceCassette1 = (100 * (fiatBalance1 / 500)).toFixed(0)
+        newConfig.notifications_fillingPercentageCassette1 = (100 * (fiatBalance1 / cassetteMaxCapacity)).toFixed(0)
+        newConfig.notifications_fiatBalanceCassette1 = null
       }
       if (fiatBalance2) {
-        config.notifications_fiatBalanceCassette2 = (100 * (fiatBalance2 / 500)).toFixed(0)
+        newConfig.notifications_fillingPercentageCassette2 = (100 * (fiatBalance2 / cassetteMaxCapacity)).toFixed(0)
+        newConfig.notifications_fiatBalanceCassette2 = null
       }
 
-      const {
-        notifications_fiatBalanceCassette1: notifications_fillingPercentageCassette1,
-        notifications_fiatBalanceCassette2: notifications_fillingPercentageCassette2,
-        ...rest
-      } = config
-
-      config = { notifications_fillingPercentageCassette1, notifications_fillingPercentageCassette2, ...rest }
-
-      config.notifications_fiatBalanceOverrides = _.map(override => {
-        if (override.fiatBalanceCassette1) {
-          override.fiatBalanceCassette1 = (100 * (override.fiatBalanceCassette1 / 500)).toFixed(0)
-        }
-        if (override.fiatBalanceCassette2) {
-          override.fiatBalanceCassette2 = (100 * (override.fiatBalanceCassette2 / 500)).toFixed(0)
-        }
-        const {
-          fiatBalanceCassette1: fillingPercentageCassette1,
-          fiatBalanceCassette2: fillingPercentageCassette2,
-          ...rest } = override
-        return { fillingPercentageCassette1, fillingPercentageCassette2, ...rest }
-      }, config.notifications_fiatBalanceOverrides)
-
-      return saveConfig(config)
-        .then(() => next())
+      if (overrides) {
+        newConfig.notifications_fiatBalanceOverrides = _.map(override => {
+          if (override.fiatBalanceCassette1) {
+            override.fiatBalanceCassette1 = (100 * (override.fiatBalanceCassette1 / cassetteMaxCapacity)).toFixed(0)
+          }
+          if (override.fiatBalanceCassette2) {
+            override.fiatBalanceCassette2 = (100 * (override.fiatBalanceCassette2 / cassetteMaxCapacity)).toFixed(0)
+          }
+          const {
+            fiatBalanceCassette1: fillingPercentageCassette1,
+            fiatBalanceCassette2: fillingPercentageCassette2,
+            ...rest } = override
+          return { fillingPercentageCassette1, fillingPercentageCassette2, ...rest }
+        }, config.notifications_fiatBalanceOverrides)
+      }
+      return saveConfig(newConfig)
+        .then(() => {})
     })
     .catch(err => {
       if (err.message === 'lamassu-server is not configured') {
@@ -47,6 +44,7 @@ exports.up = function (next) {
       return next(err)
     })
 }
+
 module.exports.down = function (next) {
   next()
 }
