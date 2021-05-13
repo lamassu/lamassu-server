4aed466ad7f63a890a11bf3d1457ebe93ab82503
csrapr <26280794+csrapr@users.noreply.github.com>
2021-05-13 16:19:39 +0100

    Chore: add tooltips

--- new-lamassu-admin/src/pages/CustomInfoRequests/Forms/Screen1Information.js
+++ new-lamassu-admin/src/pages/CustomInfoRequests/Forms/Screen1Information.js
@@ -1,14 +1,40 @@
+import { makeStyles } from '@material-ui/core/styles'
 import { Field } from 'formik'
 import React from 'react'
 import * as Yup from 'yup'
 
+import { Tooltip } from 'src/components/Tooltip'
 import TextInputFormik from 'src/components/inputs/formik/TextInput'
 import { H4, P } from 'src/components/typography'
 
+const styles = {
+  flex: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}
+
+const useStyles = makeStyles(styles)
 const Screen1Information = () => {
+  const classes = useStyles()
   return (
     <>
-      <H4>Screen 1 Information</H4> {/* TODO Add ? icon */}
+      <div className={classes.flex}>
+        <H4>Screen 1 Information</H4> {/* TODO Add ? icon */}
+        <Tooltip width={304}>
+          <P>
+            If the user reaches an amount specified by a compliance trigger that
+            uses this requirement, users will be asked if they agree on
+            providing you this information. If they agree, they'll be taken to
+            screen 2 where they can input the information to continue the
+            transaction. If they don't agree, they'll have to finalize the
+            transaction. The information you enter on this input will be what
+            users will see on the machine's screen. The title and description
+            should very clearly state what information will be requested to
+            them, and why it is necessary to proceed.
+          </P>
+        </Tooltip>
+      </div>
       <P>
         On screen 1 you will request the user if he agrees on providing this
         information, or if he wishes to terminate the transaction instead.
--- new-lamassu-admin/src/pages/CustomInfoRequests/Forms/Screen2Information.js
+++ new-lamassu-admin/src/pages/CustomInfoRequests/Forms/Screen2Information.js
@@ -1,14 +1,36 @@
+import { makeStyles } from '@material-ui/core/styles'
 import { Field } from 'formik'
 import React from 'react'
 import * as Yup from 'yup'
 
+import { Tooltip } from 'src/components/Tooltip'
 import TextInputFormik from 'src/components/inputs/formik/TextInput'
 import { H4, P } from 'src/components/typography'
+const styles = {
+  flex: {
+    display: 'flex',
+    alignItems: 'center'
+  }
+}
 
+const useStyles = makeStyles(styles)
 const ScreenInformation = () => {
+  const classes = useStyles()
+
   return (
     <>
-      <H4>Screen 2 Information</H4> {/* TODO Add ? icon */}
+      <div className={classes.flex}>
+        <H4>Screen 2 Information</H4>
+        <Tooltip width={304}>
+          <P>
+            Upon agreeing to provide the information you are requiring,
+            depending on the entry type you selected, users will be requested to
+            enter that same information on a second screen. The title you choose
+            for this screen would ideally briefly describe what the screen is
+            for, to keep users situated on the process.
+          </P>
+        </Tooltip>
+      </div>
       <P>
         If the user agrees, on screen 2 is where the user will enter the custom
         information.
