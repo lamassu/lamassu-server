a8e721f18140e2f0fc2b9871dbd426581252786a
José Oliveira <josepfo.w@gmail.com>
2021-05-14 17:05:53 +0100

    refactor: remove extra conditions and fields renaming

--- migrations/1616687417371-add_internal_tx_to_trades.js
+++ migrations/1616687417371-add_internal_tx_to_trades.js
@@ -3,14 +3,14 @@ const db = require('./db')
 exports.up = function (next) {
   var sql = [
     `CREATE TABLE cashout_tx_trades (
-      cash_out_tx_id uuid REFERENCES cash_out_txs(id),
+      tx_id uuid REFERENCES cash_out_txs(id),
       trade_id serial REFERENCES trades(id),
-      CONSTRAINT cashout_trade_pkey PRIMARY KEY (cash_out_tx_id,trade_id)
+      CONSTRAINT cashout_trade_pkey PRIMARY KEY (tx_id,trade_id)
     )`,
     `CREATE TABLE cashin_tx_trades (
-      cash_in_tx_id uuid REFERENCES cash_in_txs(id),
+      tx_id uuid REFERENCES cash_in_txs(id),
       trade_id serial REFERENCES trades(id),
-      CONSTRAINT cashin_trade_pkey PRIMARY KEY (cash_in_tx_id,trade_id)
+      CONSTRAINT cashin_trade_pkey PRIMARY KEY (tx_id,trade_id)
     )`
   ]
 
