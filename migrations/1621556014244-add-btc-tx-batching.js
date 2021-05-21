d7245a65a4db8c56a0dda27ff662dd05a223e06e
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-05-21 16:21:54 +0100

    feat: add batch_id to cash_in_txs feat: bitcoind sendmany request feat: check if wallet supports transaction batching

---
+++ migrations/1621556014244-add-btc-tx-batching.js
@@ -0,0 +1,22 @@
+var db = require('./db')
+
+exports.up = function (next) {
+  var sql = [
+    `CREATE TYPE transaction_batch_status AS ENUM('open', 'failed', 'sent')`,
+    `CREATE TABLE transaction_batches (
+      id UUID PRIMARY KEY,
+      crypto_code TEXT NOT NULL,
+      status transaction_batch_status NOT NULL DEFAULT 'open',
+      created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+      closed_at TIMESTAMPTZ,
+      error_message TEXT
+    )`,
+    `ALTER TABLE cash_in_txs ADD COLUMN batch_id REFERENCES transaction_batches(id)`
+  ]
+
+  db.multi(sql, next)
+}
+
+exports.down = function (next) {
+  next()
+}
