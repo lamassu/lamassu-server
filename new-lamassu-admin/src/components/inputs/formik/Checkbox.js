271ddfa7ec3171a96c0d6c6618bfcb08aef2e3ea
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-05-28 19:15:39 +0100

    fix: default prop values

--- new-lamassu-admin/src/components/inputs/formik/Checkbox.js
+++ new-lamassu-admin/src/components/inputs/formik/Checkbox.js
@@ -2,24 +2,33 @@ import React, { memo } from 'react'
 
 import { Checkbox } from '../base'
 
-const CheckboxInput = memo(({ label, textAlign, fullWidth, ...props }) => {
-  const { name, onChange, value } = props.field
+const CheckboxInput = memo(
+  ({
+    label,
+    textAlign,
+    fullWidth,
+    enabled = true,
+    disabledMessage = '',
+    ...props
+  }) => {
+    const { name, onChange, value } = props.field
 
-  const settings = {
-    enabled: props.enabled ?? true,
-    label: label,
-    disabledMessage: props.disabledMessage ?? ''
+    const settings = {
+      enabled: enabled,
+      label: label,
+      disabledMessage: disabledMessage
+    }
+
+    return (
+      <Checkbox
+        name={name}
+        onChange={onChange}
+        value={value}
+        settings={settings}
+        {...props}
+      />
+    )
   }
-
-  return (
-    <Checkbox
-      name={name}
-      onChange={onChange}
-      value={value}
-      settings={settings}
-      {...props}
-    />
-  )
-})
+)
 
 export default CheckboxInput
