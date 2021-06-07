bd96bcfc40258684e41930b3e44c7cb996d3b675
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-06-07 17:59:09 +0100

    fix: lamassu-coins import fix: USDT wallet splash screen

--- new-lamassu-admin/src/pages/Wallet/WizardSplash.js
+++ new-lamassu-admin/src/pages/Wallet/WizardSplash.js
@@ -8,6 +8,7 @@ import { ReactComponent as BitcoinCashLogo } from 'src/styling/logos/icon-bitcoi
 import { ReactComponent as DashLogo } from 'src/styling/logos/icon-dash-colour.svg'
 import { ReactComponent as EthereumLogo } from 'src/styling/logos/icon-ethereum-colour.svg'
 import { ReactComponent as LitecoinLogo } from 'src/styling/logos/icon-litecoin-colour.svg'
+import { ReactComponent as TetherLogo } from 'src/styling/logos/icon-tether-colour.svg'
 import { ReactComponent as ZCashLogo } from 'src/styling/logos/icon-zcash-colour.svg'
 
 const styles = {
@@ -51,7 +52,7 @@ const getLogo = code => {
     case 'ZEC':
       return ZCashLogo
     case 'USDT':
-      return EthereumLogo
+      return TetherLogo
     default:
       return null
   }
