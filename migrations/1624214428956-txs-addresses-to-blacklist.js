92c77c419491e73ae8537add74ebe2c74f61f2cc
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-06-23 18:03:51 +0100

    feat: add migration to clean blacklist table fix: address reuse and blacklist check

--- migrations/1624214428956-txs-addresses-to-blacklist.js
+++ migrations/1624214428956-txs-addresses-to-blacklist.js
@@ -2,8 +2,11 @@ var db = require('./db')
 
 exports.up = function (next) {
   var sql = [
-    `INSERT INTO blacklist SELECT DISTINCT crypto_code, to_address, false FROM cash_in_txs ON CONFLICT DO NOTHING`,
-    `INSERT INTO blacklist SELECT DISTINCT crypto_code, to_address, false FROM cash_out_txs ON CONFLICT DO NOTHING`
+    `ALTER TABLE blacklist DROP CONSTRAINT blacklist_crypto_code_address_created_by_operator_key`,
+    `ALTER TABLE blacklist ADD CONSTRAINT blacklist_crypto_code_address_key UNIQUE (crypto_code, address)`,
+    `DROP INDEX blacklist_created_by_operator_idx`,
+    `ALTER TABLE blacklist DROP COLUMN created_by_operator`,
+    `CREATE INDEX cash_in_txs_to_address_idx ON cash_in_txs(to_address)`
   ]
 
   db.multi(sql, next)
