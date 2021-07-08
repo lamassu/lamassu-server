6eaf6568e3113877905d14aa67299f2307f79493
João Leal <37.joao.leal@gmail.com>
2021-07-08 16:35:39 +0100

    fix: CSS clean up due to some orphan styling

--- new-lamassu-admin/src/components/machineActions/MachineActions.styles.js
+++ new-lamassu-admin/src/components/machineActions/MachineActions.styles.js
@@ -1,24 +1,9 @@
-import { fade } from '@material-ui/core/styles/colorManipulator'
-
 import typographyStyles from 'src/components/typography/styles'
-import {
-  offColor,
-  spacer,
-  comet,
-  primaryColor,
-  fontSize4,
-  errorColor
-} from 'src/styling/variables'
+import { offColor, spacer, errorColor } from 'src/styling/variables'
 
 const { label1 } = typographyStyles
 
 const machineActionsStyles = {
-  colDivider: {
-    width: 1,
-    margin: [[spacer * 2, spacer * 4]],
-    backgroundColor: comet,
-    border: 'none'
-  },
   label: {
     extend: label1,
     color: offColor,
@@ -33,43 +18,9 @@ const machineActionsStyles = {
     flexWrap: 'wrap',
     justifyContent: 'start'
   },
-  wrapper: {
-    display: 'flex',
-    marginTop: 12,
-    marginBottom: 16,
-    fontSize: fontSize4
-  },
-  row: {
-    display: 'flex',
-    flexDirection: 'row'
-  },
-  list: {
-    padding: 0,
-    margin: 0,
-    listStyle: 'none'
-  },
-  item: {
-    height: spacer * 3,
-    marginBottom: spacer * 1.5
-  },
-  link: {
-    color: primaryColor,
-    textDecoration: 'none'
-  },
-  divider: {
-    margin: '0 1rem'
-  },
   mr: {
     marginRight: spacer,
     marginBottom: spacer
-  },
-  separator: {
-    width: 1,
-    height: 170,
-    zIndex: 1,
-    marginRight: 60,
-    marginLeft: 'auto',
-    background: fade(comet, 0.5)
   },
   warning: {
     color: errorColor
