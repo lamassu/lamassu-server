04b23d73fcad1d179b541f694bb9bbc2777f6384
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-07-19 19:12:05 +0100

    fix: edge case with suspensions shorter than 1 day

--- new-lamassu-admin/src/pages/Customers/helper.js
+++ new-lamassu-admin/src/pages/Customers/helper.js
@@ -6,8 +6,10 @@ const CUSTOMER_BLOCKED = 'blocked'
 const getAuthorizedStatus = it =>
   it.authorizedOverride === CUSTOMER_BLOCKED
     ? { label: 'Blocked', type: 'error' }
-    : it.daysSuspended > 0
-    ? { label: `${it.daysSuspended} day suspension`, type: 'warning' }
+    : it.isSuspended
+    ? it.daysSuspended > 0
+      ? { label: `${it.daysSuspended} day suspension`, type: 'warning' }
+      : { label: `< 1 day suspension`, type: 'warning' }
     : { label: 'Authorized', type: 'success' }
 
 const getFormattedPhone = (phone, country) => {
