e3ebc9b0c733c10670640e19fe644775387a33ae
José Oliveira <josepfo.w@gmail.com>
2021-08-03 17:33:11 +0100

    fix: fetch seed from account and use db hdindex

--- bin/lamassu-eth-recovery.js
+++ bin/lamassu-eth-recovery.js
@@ -1,25 +1,42 @@
 const hdkey = require('ethereumjs-wallet/hdkey')
+const hkdf = require('futoin-hkdf')
+const db = require('../lib/db')
+const mnemonicHelpers = require('../lib/mnemonic-helpers')
+
+const pify = require('pify')
+const fs = pify(require('fs'))
+
+const options = require('../lib/options')
 
 const paymentPrefixPath = "m/44'/60'/0'/0'"
 const defaultPrefixPath = "m/44'/60'/1'/0'"
 
-function run (seed, targetAddress) {
-  const defaultTest = defaultHdNode(seed).deriveChild(0).getWallet().getChecksumAddressString()
-  console.log('Default test address: ', defaultTest)
-  const limit = 10000000
-  for (var i = 0; i <= limit; i++) {
-    const address = newAddress(seed, i)
-    console.log(address, i)
-    if (address === targetAddress) {
-      console.log('Found hdIndex: ', i)
-      break
-    }
-  }
+const address = process.argv[2]
+
+function run (address) {
+  Promise.all([fetchMnemonic(), searchForHdIndex(address)])
+    .then(([mnemonic, hdIndex]) => {
+      if (!mnemonic || !hdIndex) {
+        console.log(`Error while retrieving private key!`)
+        return
+      }
+      console.log(`Private key: `, defaultHdNode(mnemonic).deriveChild(hdIndex).getWallet().getPrivateKeyString())
+    })
 }
 
-function newAddress (seed, hdIndex) {
-  const childNode = paymentHdNode(seed).deriveChild(hdIndex)
-  return childNode.getWallet().getChecksumAddressString()
+function searchForHdIndex (address) {
+  const sql = `SELECT hd_index FROM cash_out_txs WHERE to_address = $1`
+  return db.oneOrNone(sql, [address])
+}
+
+function fetchMnemonic () {
+  return fs.readFile(options.mnemonicPath, 'utf8')
+    .then(mnemonic => computeSeed(mnemonic))
+}
+
+function computeSeed (seed) {
+  const masterSeed = mnemonicHelpers.toEntropyBuffer(seed)
+  return hkdf(masterSeed, 32, { salt: 'lamassu-server-salt', info: 'wallet-seed' })
 }
 
 function paymentHdNode (masterSeed) {
@@ -34,4 +51,4 @@ function defaultHdNode (masterSeed) {
   return key.derivePath(defaultPrefixPath)
 }
 
-run(seed, targetAddress)
+run(address)
