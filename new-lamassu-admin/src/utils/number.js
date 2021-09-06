de2a9f6e0dbcdc653e07fe3f8b05c54c62ed1e37
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-09-06 19:26:16 +0100

    chore: remove logs

--- new-lamassu-admin/src/utils/number.js
+++ new-lamassu-admin/src/utils/number.js
@@ -2,9 +2,6 @@ import * as R from 'ramda'
 
 const isValidNumber = R.both(R.is(Number), R.complement(R.equals(NaN)))
 
-const transformNumber = value => {
-  console.log('value', value)
-  return isValidNumber(value) ? value : null
-}
+const transformNumber = value => (isValidNumber(value) ? value : null)
 
 export { transformNumber }
