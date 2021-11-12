8fcc8d71bfe5ca32cc3fff4cefc395fd48aee9c6
André Sá <github-siiky@net-c.cat>
2021-11-12 14:50:47 +0000

    feat: cashbox batch table and update bills

---
+++ migrations/1616528363530-add_cashbox_batches.js
@@ -0,0 +1,26 @@
+var db = require('./db')
+
+exports.up = function (next) {
+  var sqls = [
+    `create table cashbox_batches (
+      id uuid PRIMARY KEY,
+      device_id text REFERENCES devices (device_id),
+      created timestamptz NOT NULL default now()
+    )`,
+
+    `ALTER TABLE bills ADD COLUMN legacy boolean DEFAULT false`,
+
+    `ALTER TABLE bills ADD COLUMN cashbox_batch_id uuid`,
+
+    `ALTER TABLE bills ADD CONSTRAINT cashbox_batch_id
+    FOREIGN KEY (cashbox_batch_id)
+    REFERENCES cashbox_batches (id)`,
+
+    `UPDATE bills SET legacy = 'true'`
+  ]
+  db.multi(sqls, next)
+}
+
+exports.down = function (next) {
+  next()
+}
