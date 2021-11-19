9c573a45d222a8dbc44e227c3b63b3383d0d9e9c
Sérgio Salgado <sergiodiassalgado@gmail.com>
2021-11-19 19:44:37 +0000

    fix: remove usage of env var

--- new-lamassu-admin/src/utils/urlResolver.js
+++ new-lamassu-admin/src/utils/urlResolver.js
@@ -1,7 +1,4 @@
-const url =
-  process.env.NODE_ENV === 'development'
-    ? 'https://localhost:3001'
-    : `https://${window.location.hostname}`
+const url = `https://${window.location.hostname}`
 
 const urlResolver = content => `${url}${content}`
 
