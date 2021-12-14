f5222bfea7ae3fcee89e7c5f56fccce8465955fb
André Sá <github-siiky@net-c.cat>
2021-12-14 12:25:19 +0000

    feat: backport manual/automatic cash box batch creation

--- new-lamassu-admin/src/pages/Maintenance/CashCassettes.js
+++ new-lamassu-admin/src/pages/Maintenance/CashCassettes.js
@@ -1,20 +1,25 @@
 import { useQuery, useMutation } from '@apollo/react-hooks'
-import { makeStyles } from '@material-ui/core'
+import { DialogActions, makeStyles, Box } from '@material-ui/core'
 import gql from 'graphql-tag'
 import * as R from 'ramda'
 import React, { useState } from 'react'
 import * as Yup from 'yup'
 
-import { IconButton } from 'src/components/buttons'
+import Modal from 'src/components/Modal'
+import { IconButton, Button } from 'src/components/buttons'
 import { Table as EditableTable } from 'src/components/editableTable'
+import { RadioGroup } from 'src/components/inputs'
 import { CashOut, CashIn } from 'src/components/inputs/cashbox/Cashbox'
 import { NumberInput, CashCassetteInput } from 'src/components/inputs/formik'
 import TitleSection from 'src/components/layout/TitleSection'
 import { EmptyTable } from 'src/components/table'
+import { P, Label1 } from 'src/components/typography'
 import { ReactComponent as EditIcon } from 'src/styling/icons/action/edit/enabled.svg'
 import { ReactComponent as ReverseHistoryIcon } from 'src/styling/icons/circle buttons/history/white.svg'
 import { ReactComponent as HistoryIcon } from 'src/styling/icons/circle buttons/history/zodiac.svg'
-import { fromNamespace } from 'src/utils/config'
+import { fromNamespace, toNamespace } from 'src/utils/config'
+import { MANUAL, AUTOMATIC } from 'src/utils/constants.js'
+import { onlyFirstToUpper } from 'src/utils/string'
 
 import styles from './CashCassettes.styles.js'
 import CashCassettesFooter from './CashCassettesFooter'
@@ -73,6 +78,12 @@ const GET_MACHINES_AND_CONFIG = gql`
   }
 `
 
+const SAVE_CONFIG = gql`
+  mutation Save($config: JSONObject) {
+    saveConfig(config: $config)
+  }
+`
+
 /* 
   // for cash in total calculation
   bills {
@@ -115,6 +126,8 @@ const SET_CASSETTE_BILLS = gql`
 const CashCassettes = () => {
   const classes = useStyles()
   const [showHistory, setShowHistory] = useState(false)
+  const [editingSchema, setEditingSchema] = useState(null)
+  const [selectedRadio, setSelectedRadio] = useState(null)
 
   const { data } = useQuery(GET_MACHINES_AND_CONFIG)
   const [wizard, setWizard] = useState(false)
@@ -125,6 +138,11 @@ const CashCassettes = () => {
   const [setCassetteBills, { error }] = useMutation(SET_CASSETTE_BILLS, {
     refetchQueries: () => ['getData']
   })
+  const [saveConfig] = useMutation(SAVE_CONFIG, {
+    onCompleted: () => setEditingSchema(false),
+    refetchQueries: () => ['getData']
+  })
+
   const bills = R.groupBy(bill => bill.deviceId)(R.path(['bills'])(data) ?? [])
   const deviceIds = R.uniq(
     R.map(R.prop('deviceId'))(R.path(['bills'])(data) ?? [])
@@ -140,6 +158,20 @@ const CashCassettes = () => {
   const getCashoutSettings = id => fromNamespace(id)(cashout)
   const isCashOutDisabled = ({ id }) => !getCashoutSettings(id).active
 
+  const cashboxReset =
+    data?.config && fromNamespace('cashIn')(data.config).cashboxReset
+
+  const cashboxResetSave = rawConfig => {
+    const config = toNamespace('cashIn')(rawConfig)
+    return saveConfig({ variables: { config } })
+  }
+
+  const saveCashboxOption = selection => {
+    if (selection) {
+      cashboxResetSave({ cashboxReset: selection })
+      setEditingSchema(false)
+    }
+  }
   const onSave = (id, cashbox, cassettes) => {
     return setCassetteBills({
       variables: {
@@ -149,6 +181,16 @@ const CashCassettes = () => {
         ...cassettes
       }
     })
+  }
+
+  const radioButtonOptions = [
+    { display: 'Automatic', code: AUTOMATIC },
+    { display: 'Manual', code: MANUAL }
+  ]
+
+  const handleRadioButtons = evt => {
+    const selectedRadio = R.path(['target', 'value'])(evt)
+    setSelectedRadio(selectedRadio)
   }
 
   const elements = [
@@ -232,7 +274,29 @@ const CashCassettes = () => {
           toggle: setShowHistory
         }}
         iconClassName={classes.listViewButton}
-      />
+        className={classes.tableWidth}>
+        {!showHistory && (
+          <Box alignItems="center" justifyContent="end">
+            <Label1 className={classes.cashboxReset}>Cash box reset</Label1>
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="end"
+              mr="-4px">
+              {cashboxReset && (
+                <P className={classes.selection}>
+                  {onlyFirstToUpper(cashboxReset)}
+                </P>
+              )}
+              <IconButton
+                onClick={() => setEditingSchema(true)}
+                className={classes.button}>
+                <EditIcon />
+              </IconButton>
+            </Box>
+          </Box>
+        )}
+      </TitleSection>
       <div className={classes.tableContainer}>
         {!showHistory && (
           <>
@@ -273,6 +337,46 @@ const CashCassettes = () => {
           save={onSave}
           locale={locale}
         />
+      )}
+      {editingSchema && (
+        <Modal
+          title={'Cash box reset'}
+          width={478}
+          handleClose={() => setEditingSchema(null)}
+          open={true}>
+          <P className={classes.descriptions}>
+            Specify if you want your cash-in counts to be reset automatically or
+            manually.
+          </P>
+          <RadioGroup
+            name="set-automatic-reset"
+            value={selectedRadio ?? cashboxReset}
+            options={[radioButtonOptions[0]]}
+            onChange={handleRadioButtons}
+            className={classes.radioButtons}
+          />
+          <P className={classes.descriptions}>
+            Choose this option if you want your cash-in cash box count to be
+            reset automatically when it is physically removed from the machine.
+          </P>
+          <RadioGroup
+            name="set-manual-reset"
+            value={selectedRadio ?? cashboxReset}
+            options={[radioButtonOptions[1]]}
+            onChange={handleRadioButtons}
+            className={classes.radioButtons}
+          />
+          <P className={classes.descriptions}>
+            Choose this option if you want to edit your cash-in counts manually
+            on Lamassu Admin, after you physically remove the bills from the
+            cash box.
+          </P>
+          <DialogActions className={classes.actions}>
+            <Button onClick={() => saveCashboxOption(selectedRadio)}>
+              Confirm
+            </Button>
+          </DialogActions>
+        </Modal>
       )}
     </>
   )
--- new-lamassu-admin/src/pages/Maintenance/CashCassettes.styles.js
+++ new-lamassu-admin/src/pages/Maintenance/CashCassettes.styles.js
@@ -1,3 +1,5 @@
+import { offColor } from 'src/styling/variables'
+
 export default {
   cashbox: {
     height: 36
@@ -9,5 +11,21 @@ export default {
   tBody: {
     maxHeight: '65vh',
     overflow: 'auto'
+  },
+  tableWidth: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: 1
+  },
+  descriptions: {
+    color: offColor,
+    marginTop: 0
+  },
+  cashboxReset: {
+    color: offColor,
+    margin: [[13, 0, -5, 20]]
+  },
+  selection: {
+    marginRight: 12
   }
 }
