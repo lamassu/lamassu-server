6f0d76de9b8cb72bd24025afea14e8f7dae09135
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-01-17 23:22:30 +0000

    fix: limit transactions from analytics page to the last 2 months

--- migrations/1641394367865-testing-customer-toggle.js
+++ migrations/1641394367865-testing-customer-toggle.js
@@ -2,7 +2,7 @@ var db = require('./db')
 
 exports.up = function (next) {
   var sql = [
-    `ALTER TABLE customers ADD COLUMN is_test_customer BOOLEAN DEFAULT false`,
+    `ALTER TABLE customers ADD COLUMN is_test_customer BOOLEAN NOT NULL DEFAULT false`,
   ]
 
   db.multi(sql, next)
