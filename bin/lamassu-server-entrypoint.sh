1e142c36b21d5b8fe188bd47170f55c7db99676a
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-04-06 16:32:37 +0100

    fix: script order

--- bin/lamassu-server-entrypoint.sh
+++ bin/lamassu-server-entrypoint.sh
@@ -7,14 +7,14 @@ FILE_4=/etc/ssl/certs/Lamassu_OP_Root_CA.srl
 FILE_5=/etc/ssl/private/Lamassu_OP.key
 FILE_6=/etc/ssl/private/Lamassu_OP_Root_CA.key
 
-echo "Executing migrations..."
-node /app/packages/lamassu-server/bin/lamassu-migrate
-
 echo "Checking for the existence of certificates..."
 if [[ ! -f "$FILE_1" || ! -f "$FILE_2" || ! -f "$FILE_3" || ! -f "$FILE_4" || ! -f "$FILE_5" || ! -f "$FILE_6" ]]; then
   echo "No Lamassu certificates found. Building them..."
   bash /app/packages/lamassu-server/tools/build-docker-certs.sh
 fi
+
+echo "Executing migrations..."
+node /app/packages/lamassu-server/bin/lamassu-migrate
 
 USER_COUNT=$(node /app/packages/lamassu-server/tools/check-user-count.js)
 
