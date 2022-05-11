056ce5093c17240bb7b5536ebc4b8cd9b02ca378
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-05-11 19:14:07 +0100

    fix: production environment .env file path

--- tools/check-user-count.js
+++ tools/check-user-count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? path.resolve('/etc', 'lamassu', '.env') : path.resolve(__dirname, '../.env') })
 const db = require('../lib/db')
 
 const getCount = () => {
