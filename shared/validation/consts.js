acf7c125bee58520dfa24027bf9ca6d923b55d6a
José Oliveira <josepfo.w@gmail.com>
2022-05-19 18:47:54 +0100

    feat: new admin schemas

--- shared/validation/consts.js
+++ shared/validation/consts.js
@@ -1,5 +1,44 @@
+const PASSWORD_MIN_LENGTH = 8
 const CURRENCY_MAX = 9999999
+const percentMax = 100
+const percentMin = 0
+const notesMin = 0
+
+const CASSETTE_1_KEY = 'fillingPercentageCassette1'
+const CASSETTE_2_KEY = 'fillingPercentageCassette2'
+const CASSETTE_3_KEY = 'fillingPercentageCassette3'
+const CASSETTE_4_KEY = 'fillingPercentageCassette4'
+const MACHINE_KEY = 'machine'
+
+const CASSETTE_LIST = [
+  CASSETTE_1_KEY,
+  CASSETTE_2_KEY,
+  CASSETTE_3_KEY,
+  CASSETTE_4_KEY
+]
+
+const ALL_MACHINES = {
+  name: 'All Machines',
+  deviceId: 'ALL_MACHINES'
+}
+
+const ALL_COINS = {
+  display: 'All Coins',
+  code: 'ALL_COINS'
+}
 
 export {
-  CURRENCY_MAX
+  CURRENCY_MAX,
+  PASSWORD_MIN_LENGTH,
+  percentMax,
+  percentMin,
+  notesMin,
+  ALL_COINS,
+  ALL_MACHINES,
+  CASSETTE_LIST,
+  CASSETTE_1_KEY,
+  CASSETTE_2_KEY,
+  CASSETTE_3_KEY,
+  CASSETTE_4_KEY,
+  MACHINE_KEY
 }
--- shared/validation/utils.js
+++ shared/validation/utils.js
@@ -1,6 +1,9 @@
 const Yup = require('yup')
+const R = require('ramda')
 
 const transformNumber = value => (isValidNumber(value) ? value : null)
+const highestBill = bills => R.isEmpty(bills) ? CURRENCY_MAX : Math.max(...bills)
+const isDefined = it => it && it.length
 
 const PREFILL = {
     smsCode: {
@@ -30,7 +33,86 @@ const PREFILL = {
     }
 }
 
+const getAlreadyUsed = (id, machine, values) => {
+  const getCrypto = R.prop('cryptoCurrencies')
+  const getMachineId = R.prop('machine')
+
+  const filteredOverrides = R.filter(R.propEq('machine', machine))(values)
+  const originalValue = R.find(R.propEq('id', id))(values)
+
+  const originalCryptos = getCrypto(originalValue)
+  const originalMachineId = getMachineId(originalValue)
+
+  const alreadyUsed = R.compose(
+    R.uniq,
+    R.flatten,
+    R.map(getCrypto)
+  )(filteredOverrides)
+
+  if (machine !== originalMachineId) return alreadyUsed ?? []
+
+  return R.difference(alreadyUsed, originalCryptos)
+}
+
+const buildTestValidation = (id, passphrase) => {
+  return Yup.string()
+    .max(100, 'Too long')
+    .when(id, {
+      is: isDefined,
+      then: Yup.string().test(secretTest(passphrase))
+    })
+}
+
+const secretTest = (secret, message) => ({
+  name: 'secret-test',
+  message: message ? `The ${message} is invalid` : 'Invalid field',
+  test(val) {
+    if (R.isNil(secret) && R.isNil(val)) {
+      return this.createError()
+    }
+    return true
+  }
+})
+
+const leadingZerosTest = (value, context) => {
+  if (
+    R.startsWith('0', context.originalValue) &&
+    R.length(context.originalValue) > 1
+  ) {
+    return context.createError()
+  }
+  return true
+}
+
+const triggerType = Yup.string().required()
+const threshold = Yup.object().shape({
+  threshold: Yup.number()
+    .nullable()
+    .transform(transformNumber)
+    .label('Invalid threshold'),
+  thresholdDays: Yup.number()
+    .transform(transformNumber)
+    .nullable()
+    .label('Invalid threshold days')
+})
+
+const requirement = Yup.object().shape({
+  requirement: Yup.string().required(),
+  suspensionDays: Yup.number()
+    .transform(transformNumber)
+    .nullable()
+})
+
 module.exports = {
   transformNumber,
-  PREFILL
+  PREFILL,
+  getAlreadyUsed,
+  highestBill,
+  buildTestValidation,
+  secretTest,
+  leadingZerosTest,
+  isDefined,
+  triggerType,
+  threshold,
+  requirement
 }
