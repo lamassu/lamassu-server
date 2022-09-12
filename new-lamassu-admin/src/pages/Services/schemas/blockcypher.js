19fcf0cbb28109903834573755d8fcda3622dd6a
André Sá <github-siiky@net-c.cat>
2022-09-12 17:10:56 +0100

    fix: use the correct input component for the Blockcypher confidence factor

--- new-lamassu-admin/src/pages/Services/schemas/blockcypher.js
+++ new-lamassu-admin/src/pages/Services/schemas/blockcypher.js
@@ -1,7 +1,6 @@
 import * as Yup from 'yup'
 
-import CheckboxInput from 'src/components/inputs/formik/Checkbox'
-import TextInputFormik from 'src/components/inputs/formik/TextInput'
+import { Checkbox, TextInput, NumberInput } from 'src/components/inputs/formik'
 
 export default {
   code: 'blockcypher',
@@ -11,19 +10,19 @@ export default {
     {
       code: 'token',
       display: 'API Token',
-      component: TextInputFormik,
+      component: TextInput,
       face: true,
       long: true
     },
     {
       code: 'confidenceFactor',
       display: 'Confidence Factor',
-      component: TextInputFormik,
+      component: NumberInput,
       face: true
     },
     {
       code: 'rbf',
-      component: CheckboxInput,
+      component: Checkbox,
       settings: {
         field: 'wallets_BTC_wallet',
         enabled: true,
