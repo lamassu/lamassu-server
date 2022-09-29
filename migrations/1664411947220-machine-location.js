8939e0170535cfd50875ef7491dc5a5a9abb867b
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-09-29 05:03:38 +0100

    feat: add machine location step to new machine form

---
+++ migrations/1664411947220-machine-location.js
@@ -0,0 +1,22 @@
+var db = require('./db')
+
+exports.up = function (next) {
+  var sql = [
+    `CREATE TABLE machine_locations (
+      id UUID PRIMARY KEY,
+      label TEXT NOT NULL,
+      address_line_1 TEXT NOT NULL,
+      address_line_2 TEXT,
+      zip_code TEXT NOT NULL,
+      country TEXT NOT NULL
+    )`,
+    `ALTER TABLE devices ADD COLUMN machine_location UUID REFERENCES machine_locations(id)`,
+    `ALTER TABLE pairing_tokens ADD COLUMN machine_location UUID REFERENCES machine_locations(id)`
+  ]
+
+  db.multi(sql, next)
+}
+
+exports.down = function (next) {
+  next()
+}
