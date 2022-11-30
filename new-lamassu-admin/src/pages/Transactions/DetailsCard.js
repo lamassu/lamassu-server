985e0aae497a051ca8748d991c346f1f44b2573d
Sérgio Salgado <sergiodiassalgado@gmail.com>
2022-11-30 18:58:56 +0000

    fix: exchange rate display in transaction card

--- new-lamassu-admin/src/pages/Transactions/DetailsCard.js
+++ new-lamassu-admin/src/pages/Transactions/DetailsCard.js
@@ -125,7 +125,9 @@ const DetailsRow = ({ it: tx, timezone }) => {
   const commissionPercentage =
     Number.parseFloat(tx.commissionPercentage, 2) * 100
   const cashInFee = isCashIn ? Number.parseFloat(tx.cashInFee) : 0
-  const fiat = Number.parseFloat(tx.fiat)
+  const fiat = BigNumber(tx.fiat)
+    .minus(cashInFee)
+    .toFixed(2, 1) // ROUND_DOWN
   const crypto = getCryptoAmount(tx)
   const exchangeRate = (fiat / crypto).toFixed(2)
   const displayExRate = `1 ${tx.cryptoCode} = ${exchangeRate} ${tx.fiatCode}`
