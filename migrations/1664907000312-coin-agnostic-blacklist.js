c3fad7188acbee1fdf69a286b2a583bc0b106200
siiky <github-siiky@net-c.cat>
2022-12-16 12:32:38 +0000

    fix: syntax error in migration file

--- migrations/1664907000312-coin-agnostic-blacklist.js
+++ migrations/1664907000312-coin-agnostic-blacklist.js
@@ -4,7 +4,7 @@ exports.up = function (next) {
   var sql = [
     `CREATE TABLE blacklist_temp (
       address TEXT NOT NULL UNIQUE
-    )`
+    )`,
     `INSERT INTO blacklist_temp (address) SELECT DISTINCT address FROM blacklist`,
     `DROP TABLE blacklist`,
     `ALTER TABLE blacklist_temp RENAME TO blacklist`
