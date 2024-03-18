5264e769878477a05cd837ddb7b37a80232cabce
Rafael Taranto <rafael.taranto@protonmail.com>
2024-03-18 09:48:34 +0000

    fix: profit display on admin

--- tools/build-dev-env.js
+++ tools/build-dev-env.js
@@ -6,7 +6,6 @@ const setEnvVariable = require('./set-env-var')
 
 fs.copyFileSync(path.resolve(__dirname, '../.sample.env'), path.resolve(__dirname, '../.env'))
 
-setEnvVariable('LAMASSU_DB', 'DEV')
 setEnvVariable('POSTGRES_USER', 'postgres')
 setEnvVariable('POSTGRES_PASSWORD', 'postgres123')
 setEnvVariable('POSTGRES_HOST', 'localhost')
