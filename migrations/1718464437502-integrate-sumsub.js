92701ce7aa9cb5dd4560f5f6e190047a577a0aa4
Rafael Taranto <rafael.taranto@protonmail.com>
2024-06-18 23:43:35 +0100

    feat: notifications and flow fixes

--- migrations/1718464437502-integrate-sumsub.js
+++ migrations/1718464437502-integrate-sumsub.js
@@ -2,7 +2,7 @@ const db = require('./db')
 
 exports.up = function (next) {
   let sql = [
-    `CREATE TYPE EXTERNAL_COMPLIANCE_STATUS AS ENUM('WAIT', 'APPROVED', 'REJECTED', 'RETRY')`,
+    `CREATE TYPE EXTERNAL_COMPLIANCE_STATUS AS ENUM('PENDING', 'APPROVED', 'REJECTED', 'RETRY')`,
     `CREATE TABLE CUSTOMER_EXTERNAL_COMPLIANCE (
       customer_id UUID NOT NULL REFERENCES customers(id),
       service TEXT NOT NULL,
